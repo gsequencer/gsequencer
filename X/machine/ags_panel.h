4c13fd7e39fb61fa86f97c05654066b10f842e6f
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:39:20 +0000

    my first import

---
+++ X/machine/ags_panel.h
@@ -0,0 +1,33 @@
+#ifndef __AGS_PANEL_H__
+#define __AGS_PANEL_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+#include "../ags_machine.h"
+
+#define AGS_TYPE_PANEL                (ags_panel_get_type())
+#define AGS_PANEL(obj)                ((AgsPanel*) G_TYPE_CHECK_INSTANCE_CAST(obj, AGS_TYPE_PANEL, AgsPanel))
+#define AGS_PANEL_CLASS(class)        ((AgsPanelClass*) G_TYPE_CHECK_CLASS_CAST(class, AGS_TYPE_PANEL, AgsPanelClass))
+#define AGS_IS_PANEL(obj)             (G_TYPE_CHECK_INSTANCE_TYPE(obj, AGS_TYPE_PANEL))
+#define AGS_IS_PANEL_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE((class), AGS_TYPE_PANEL))
+#define AGS_PANEL_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS((obj), AGS_TYPE_PANEL, AgsPanelClass))
+
+typedef struct _AgsPanel AgsPanel;
+typedef struct _AgsPanelClass AgsPanelClass;
+
+struct _AgsPanel
+{
+  AgsMachine machine;
+
+  GtkVBox *vbox;
+};
+
+struct _AgsPanelClass
+{
+  AgsMachineClass machine;
+};
+
+AgsPanel* ags_panel_new();
+
+#endif /*__AGS_PANEL_H__*/
