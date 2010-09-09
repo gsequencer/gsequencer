d991c0b3c3607929a2b825df1b8d68c46b71385a
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:45:09 +0000

    my first import

---
+++ X/machine/ags_mixer_callbacks.c
@@ -0,0 +1,24 @@
+#include "ags_mixer_callbacks.h"
+
+#include "../ags_window.h"
+
+void
+ags_mixer_parent_set_callback(GtkWidget *widget, GtkObject *old_parent, AgsMixer *mixer)
+{
+  AgsWindow *window;
+
+  if(old_parent != NULL)
+    return;
+
+  window = (AgsWindow *) gtk_widget_get_toplevel(widget);
+  mixer->machine.name = g_strdup_printf("Default %d\0", window->counter->mixer);
+  window->counter->mixer++;
+}
+
+gboolean
+ags_mixer_destroy_callback(GtkObject *object, AgsMixer *mixer)
+{
+  ags_mixer_destroy(object);
+
+  return(TRUE);
+}
