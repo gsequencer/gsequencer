218f9a52ae15e93397795853b68696b3ec9d2383
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:28:10 +0000

    my first import

---
+++ X/ags_machine_line_member_editor_callbacks.h
@@ -0,0 +1,10 @@
+#ifndef __AGS_MACHINE_LINE_MEMBER_EDITOR_H__
+#define __AGS_MACHINE_LINE_MEMBER_EDITOR_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "ags_machine_line_member_editor.h"
+
+#endif /*__AGS_MACHINE_LINE_MEMBER_EDITOR_H__*/
