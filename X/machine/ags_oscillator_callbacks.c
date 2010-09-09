30f4d91e26a2e40f8d29d40cbe452f26208c271a
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:37:40 +0000

    my first import

---
+++ X/machine/ags_oscillator_callbacks.c
@@ -0,0 +1,38 @@
+#include "ags_oscillator_callbacks.h"
+#include "ags_synth.h"
+
+void
+ags_oscillator_wave_callback(GtkOptionMenu *option, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
+
+void
+ags_oscillator_attack_callback(GtkSpinButton *spin_button, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
+
+void
+ags_oscillator_length_callback(GtkSpinButton *spin_button, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
+
+void
+ags_oscillator_frequency_callback(GtkSpinButton *spin_button, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
+
+void
+ags_oscillator_phase_callback(GtkSpinButton *spin_button, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
+
+void
+ags_oscillator_volume_callback(GtkSpinButton *spin_button, AgsOscillator *oscillator)
+{
+  //  ags_synth_update(gtk_widget_get_ancestor((GtkWidget *) oscillator, AGS_TYPE_SYNTH));
+}
