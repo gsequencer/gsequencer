90e7f1cccafa5b222238fda50ff43f6e7929f86e
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:46:08 +0000

    my first import

---
+++ X/editor/ags_ruler_callbacks.h
@@ -0,0 +1,13 @@
+#ifndef __AGS_RULER_CALLBACKS_H__
+#define __AGS_RULER_CALLBACKS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "ags_ruler.h"
+
+gboolean ags_ruler_expose_event(GtkWidget *widget, GdkEventExpose *event, AgsRuler *ruler); 
+gboolean ags_ruler_configure_event(GtkWidget *widget, GdkEventConfigure *event, AgsRuler *ruler); 
+
+#endif /*__AGS_RULER_CALLBACKS_H__*/
