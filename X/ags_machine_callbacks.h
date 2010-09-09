f4eada44e71f6806c438ea364c3ded90d818ea83
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:30:26 +0000

    my first import

---
+++ X/ags_machine_callbacks.h
@@ -0,0 +1,22 @@
+#ifndef __AGS_MACHINE_CALLBACKS_H__
+#define __AGS_MACHINE_CALLBACKS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+#include "ags_machine.h"
+
+int ags_machine_parent_set_callback(GtkWidget *widget, GtkObject *old_parent, AgsMachine *machine);
+int ags_machine_destroy_callback(GtkObject *object, AgsMachine *machine);
+
+int ags_machine_button_press_callback(GtkWidget *handle_box, GdkEventButton *event, AgsMachine *machine);
+
+int ags_machine_popup_move_up_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_move_down_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_hide_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_show_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_destroy_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_rename_activate_callback(GtkWidget *widget, AgsMachine *machine);
+int ags_machine_popup_properties_activate_callback(GtkWidget *widget, AgsMachine *machine);
+
+#endif /*__AGS_MACHINE_CALLBACKS_H__*/
