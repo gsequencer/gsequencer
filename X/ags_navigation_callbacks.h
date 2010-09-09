1489abe5585c1ea60d7158a43227a2a74f3a108c
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:28:58 +0000

    my first import

---
+++ X/ags_navigation_callbacks.h
@@ -0,0 +1,40 @@
+#ifndef __AGS_NAVIGATION_CALLBACKS_H__
+#define __AGS_NAVIGATION_CALLBACKS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "ags_navigation.h"
+
+gboolean ags_navigation_destroy_callback(GtkObject *object, AgsNavigation *navigation);
+void ags_navigation_show_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_expander_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_bpm_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_rewind_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_prev_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_play_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_stop_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_next_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_forward_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_loop_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_position_sec_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_position_min_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_duration_sec_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_duration_min_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_loop_left_sec_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_loop_left_min_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_loop_right_sec_callback(GtkWidget *widget, AgsNavigation *navigation);
+void ags_navigation_loop_right_min_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+void ags_navigation_raster_callback(GtkWidget *widget, AgsNavigation *navigation);
+
+#endif /*__AGS_NAVIGATION_CALLBACKS_H__*/
