3299f6ba7d73a076762cba97bb0c2f7721a0b1a3
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:29:57 +0000

    my first import

---
+++ X/ags_window_callbacks.h
@@ -0,0 +1,13 @@
+#ifndef __AGS_WINDOW_CALLBACKS_H__
+#define __AGS_WINDOW_CALLBACKS_H__
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "ags_window.h"
+
+gboolean ags_window_delete_event_callback(GtkWidget *widget, AgsWindow *window);
+gboolean ags_window_destroy_callback(GtkObject *object, AgsWindow *window);
+void ags_window_show_callback(GtkWidget *widget, AgsWindow *window);
+
+#endif /*__AGS_WINDOW_CALLBACKS_H__*/
