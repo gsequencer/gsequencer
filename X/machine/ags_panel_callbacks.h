347854ddb4c0acf46c477ee908b3fec42fc28a41
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:40:35 +0000

    my first import

---
+++ X/machine/ags_panel_callbacks.h
@@ -0,0 +1,14 @@
+#ifndef __AGS_PANEL_CALLBACKS_H__
+#define __AGS_PANEL_CALLBACKS_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#include "ags_panel.h"
+
+void ags_panel_parent_set_callback(GtkWidget *widget, GtkObject *old_parent, AgsPanel *panel);
+
+gboolean ags_panel_destroy_callback();
+
+#endif /*__AGS_PANEL_CALLBACKS_H__*/
