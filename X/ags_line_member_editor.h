3e39116510163de57e4f8567a204e84a650ed070
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:26:47 +0000

    my first import

---
+++ X/ags_line_member_editor.h
@@ -0,0 +1,35 @@
+#ifndef __AGS_LINEMEMBEREDITOR_H__
+#define __AGS_LINEMEMBEREDITOR_H__
+
+#include <glib.h>
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#define AGS_TYPE_LINE_MEMBER_EDITOR                (ags_line_member_editor_get_type())
+#define AGS_LINE_MEMBER_EDITOR(obj)                (G_TYPE_CHECK_INSTANCE_CAST((obj), AGS_TYPE_LINE_MEMBER_EDITOR, AgsLineMemberEditor))
+#define AGS_LINE_MEMBER_EDITOR_CLASS(class)        (G_TYPE_CHECK_CLASS_CAST((class), AGS_TYPE_LINE_MEMBER_EDITOR, AgsLineMemberEditorClass))
+#define AGS_IS_LINE_MEMBER_EDITOR(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_LINE_MEMBER_EDITOR))
+#define AGS_IS_LINE_MEMBER_EDITOR_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_LINE_MEMBER_EDITOR))
+#define AGS_LINE_MEMBER_EDITOR_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS ((obj), AGS_TYPE_LINE_MEMBER_EDITOR, AgsLineMemberEditorClass))
+
+typedef struct _AgsLineMemberEditor AgsLineMemberEditor;
+typedef struct _AgsLineMemberEditorClass AgsLineMemberEditorClass;
+
+struct _AgsLineMemberEditor
+{
+  GtkVBox vbox;
+
+  GtkVBox *line_member;
+
+  GtkButton *add;
+  GtkButton *remove;
+};
+
+struct _AgsLineMemberEditorClass
+{
+  GtkVBoxClass vbox;
+};
+
+AgsLineMemberEditor* ags_line_member_editor_new();
+
+#endif /*__AGS_LINE_MEMBER_EDITOR_H__*/
