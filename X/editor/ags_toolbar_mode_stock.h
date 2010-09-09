2ef83259d395673f05fe6cda35e30111175c9cc6
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:47:55 +0000

    my first import

---
+++ X/editor/ags_toolbar_mode_stock.h
@@ -0,0 +1,7 @@
+#ifndef __AGS_TOOLBAR_MODE_STOCK_H__
+#define __AGS_TOOLBAR_MODE_STOCK_H__
+
+#define AGS_TOOLBAR_MODE_DEFAULT          "default\0"
+#define AGS_TOOLBAR_MODE_LOCK_CHANNELS    "lock channels\0"
+
+#endif /*__AGS_TOOLBAR_MODE_STOCK_H__*/
