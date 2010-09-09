b5f3bb9e7befbbba8bcc8769c9c1b5f6e3c69219
Joël Krähemann <jkraehemann@gmail.com>
2010-09-09 18:33:26 +0000

    my first import

---
+++ X/ags_line_member_editor_callbacks.c
@@ -0,0 +1,15 @@
+#include "ags_line_member_editor_callbacks.h"
+
+int
+ags_line_member_editor_destroy_callback(GtkObject *object, AgsLineMemberEditor *line_member_editor)
+{
+  ags_line_member_editor_destroy(object);
+  return(0);
+}
+
+int
+ags_line_member_editor_show_callback(GtkWidget *widget, AgsLineMemberEditor *line_member_editor)
+{
+  ags_line_member_editor_show(widget);
+  return(0);
+}
