225d847f0774ad89bd7ccf8cb9cd0db0dd0d6beb
Joël Krähemann <jkraehemann@gmail.com>
2013-09-22 22:33:35 +0000

    adding AgsSingleThread skelleton

---
+++ src/ags/thread/ags_single_thread.c
@@ -0,0 +1,160 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2005-2011 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <ags/thread/ags_single_thread.h>
+
+#include <ags-lib/object/ags_connectable.h>
+
+void ags_single_thread_class_init(AgsSingleThreadClass *single_thread);
+void ags_single_thread_connectable_interface_init(AgsConnectableInterface *connectable);
+void ags_single_thread_init(AgsSingleThread *single_thread);
+void ags_single_thread_connect(AgsConnectable *connectable);
+void ags_single_thread_disconnect(AgsConnectable *connectable);
+void ags_single_thread_finalize(GObject *gobject);
+
+void ags_single_thread_start(AgsThread *thread);
+void ags_single_thread_run(AgsThread *thread);
+void ags_single_thread_stop(AgsThread *thread);
+
+static gpointer ags_single_thread_parent_class = NULL;
+static AgsConnectableInterface *ags_single_thread_parent_connectable_interface;
+
+GType
+ags_single_thread_get_type()
+{
+  static GType ags_type_single_thread = 0;
+
+  if(!ags_type_single_thread){
+    static const GTypeInfo ags_single_thread_info = {
+      sizeof (AgsSingleThreadClass),
+      NULL, /* base_init */
+      NULL, /* base_finalize */
+      (GClassInitFunc) ags_single_thread_class_init,
+      NULL, /* class_finalize */
+      NULL, /* class_data */
+      sizeof (AgsSingleThread),
+      0,    /* n_preallocs */
+      (GInstanceInitFunc) ags_single_thread_init,
+    };
+
+    static const GInterfaceInfo ags_connectable_interface_info = {
+      (GInterfaceInitFunc) ags_single_thread_connectable_interface_init,
+      NULL, /* interface_finalize */
+      NULL, /* interface_data */
+    };
+
+    ags_type_single_thread = g_type_register_static(AGS_TYPE_THREAD,
+						    "AgsSingleThread\0",
+						    &ags_single_thread_info,
+						    0);
+    
+    g_type_add_interface_static(ags_type_single_thread,
+				AGS_TYPE_CONNECTABLE,
+				&ags_connectable_interface_info);
+  }
+  
+  return (ags_type_single_thread);
+}
+
+void
+ags_single_thread_class_init(AgsSingleThreadClass *single_thread)
+{
+  GObjectClass *gobject;
+  AgsThreadClass *thread;
+
+  ags_single_thread_parent_class = g_type_class_peek_parent(single_thread);
+
+  /* GObject */
+  gobject = (GObjectClass *) single_thread;
+
+  gobject->finalize = ags_single_thread_finalize;
+
+  /* AgsThread */
+  thread = (AgsThreadClass *) single_thread;
+
+  thread->start = ags_single_thread_start;
+  thread->run = ags_single_thread_run;
+  thread->stop = ags_single_thread_stop;
+}
+
+void
+ags_single_thread_connectable_interface_init(AgsConnectableInterface *connectable)
+{
+  ags_single_thread_parent_connectable_interface = g_type_interface_peek_parent(connectable);
+
+  connectable->connect = ags_single_thread_connect;
+  connectable->disconnect = ags_single_thread_disconnect;
+}
+
+void
+ags_single_thread_init(AgsSingleThread *single_thread)
+{
+}
+
+void
+ags_single_thread_connect(AgsConnectable *connectable)
+{
+  ags_single_thread_parent_connectable_interface->connect(connectable);
+
+  /* empty */
+}
+
+void
+ags_single_thread_disconnect(AgsConnectable *connectable)
+{
+  ags_single_thread_parent_connectable_interface->disconnect(connectable);
+
+  /* empty */
+}
+
+void
+ags_single_thread_finalize(GObject *gobject)
+{
+  G_OBJECT_CLASS(ags_single_thread_parent_class)->finalize(gobject);
+
+  /* empty */
+}
+
+void
+ags_single_thread_start(AgsThread *thread)
+{
+}
+
+void
+ags_single_thread_run(AgsThread *thread)
+{
+}
+
+void
+ags_single_thread_stop(AgsThread *thread)
+{
+}
+
+AgsSingleThread*
+ags_single_thread_new(GObject *single)
+{
+  AgsSingleThread *single_thread;
+
+  single_thread = (AgsSingleThread *) g_object_new(AGS_TYPE_SINGLE_THREAD,
+						   NULL);
+
+
+  return(single_thread);
+}
+
+
---
+++ src/ags/thread/ags_single_thread.h
@@ -0,0 +1,57 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2005-2011 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __AGS_SINGLE_THREAD_H__
+#define __AGS_SINGLE_THREAD_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <ags/thread/ags_thread.h>
+
+#include <unistd.h>
+
+#define AGS_TYPE_SINGLE_THREAD                (ags_single_thread_get_type())
+#define AGS_SINGLE_THREAD(obj)                (G_TYPE_CHECK_INSTANCE_CAST((obj), AGS_TYPE_SINGLE_THREAD, AgsSingleThread))
+#define AGS_SINGLE_THREAD_CLASS(class)        (G_TYPE_CHECK_CLASS_CAST(class, AGS_TYPE_SINGLE_THREAD, AgsSingleThreadClass))
+#define AGS_IS_SINGLE_THREAD(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_SINGLE_THREAD))
+#define AGS_IS_SINGLE_THREAD_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_SINGLE_THREAD))
+#define AGS_SINGLE_THREAD_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS(obj, AGS_TYPE_SINGLE_THREAD, AgsSingleThreadClass))
+
+typedef struct _AgsSingleThread AgsSingleThread;
+typedef struct _AgsSingleThreadClass AgsSingleThreadClass;
+
+struct _AgsSingleThread
+{
+  AgsThread thread;
+
+  gdouble frequency;
+  gdouble iter;
+};
+
+struct _AgsSingleThreadClass
+{
+  AgsThreadClass thread;
+};
+
+GType ags_single_thread_get_type();
+
+AgsSingleThread* ags_single_thread_new();
+
+#endif /*__AGS_SINGLE_THREAD_H__*/
+
