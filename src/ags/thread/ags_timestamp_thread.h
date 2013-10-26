a329a5ad09dc99e8c5298170177ca6384b8fbb9f
Joël Krähemann <jkraehemann@gmail.com>
2013-10-26 14:05:23 +0000

    add AgsTimestampThread skelleton

---
+++ src/ags/thread/ags_timestamp_thread.h
@@ -0,0 +1,52 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2013 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __AGS_TIMESTAMP_THREAD_H__
+#define __AGS_TIMESTAMP_THREAD_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#include <ags/thread/ags_thread.h>
+
+#define AGS_TYPE_TIMESTAMP_THREAD                (ags_timestamp_thread_get_type())
+#define AGS_TIMESTAMP_THREAD(obj)                (G_TYPE_CHECK_INSTANCE_CAST((obj), AGS_TYPE_TIMESTAMP_THREAD, AgsTimestampThread))
+#define AGS_TIMESTAMP_THREAD_CLASS(class)        (G_TYPE_CHECK_CLASS_CAST(class, AGS_TYPE_TIMESTAMP_THREAD, AgsTimestampThread))
+#define AGS_IS_TIMESTAMP_THREAD(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_TIMESTAMP_THREAD))
+#define AGS_IS_TIMESTAMP_THREAD_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_TIMESTAMP_THREAD))
+#define AGS_TIMESTAMP_THREAD_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS(obj, AGS_TYPE_TIMESTAMP_THREAD, AgsTimestampThreadClass))
+
+typedef struct _AgsTimestampThread AgsTimestampThread;
+typedef struct _AgsTimestampThreadClass AgsTimestampThreadClass;
+
+struct _AgsTimestampThread
+{
+  AgsThread thread;
+};
+
+struct _AgsTimestampThreadClass
+{
+  AgsThreadClass thread;
+};
+
+GType ags_timestamp_thread_get_type();
+
+AgsTimestampThread* ags_timestamp_thread_new();
+
+#endif /*__AGS_TIMESTAMP_THREAD_H__*/
+
