3842a1c231fbfe5a3ab40b8efa1237dcb8db527c
Joël Krähemann <jkraehemann@gmail.com>
2014-09-13 16:12:55 +0000

    fixed scale translation in ags_dial.c

--- src/ags/widget/ags_dial.c
+++ src/ags/widget/ags_dial.c
@@ -135,7 +135,7 @@ ags_dial_init(AgsDial *dial)
   dial->adjustment = NULL;
 
   dial->radius = 8;
-  dial->scale_precision = 8;
+  dial->scale_precision = AGS_DIAL_DEFAULT_PRECISION;
   dial->outline_strength = 4;
 
   dial->font_size = 14;
@@ -704,7 +704,7 @@ ags_dial_draw(AgsDial *dial)
     range = (dial->adjustment->upper - dial->adjustment->lower);
   }
 
-  translated_value = (gdouble) scale_precision / AGS_DIAL_DEFAULT_PRECISION * dial->adjustment->value;
+  translated_value = (gdouble) scale_precision * dial->adjustment->value / range;
 
   //  g_message("value: %f\nupper: %f\ntranslated_value: %f\n\0", GTK_RANGE(dial)->adjustment->value, GTK_RANGE(dial)->adjustment->upper, translated_value);
   cairo_set_line_width(cr, 4.0);
