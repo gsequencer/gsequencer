edeb51511e56c9d32afcea9d57b6033febda98d9
Joël Krähemann <jkraehemann@gmail.com>
2015-04-02 15:01:23 +0000

    refactoring by AgsApplicationContext: adding contices

--- src/ags/main.c
+++ src/ags/main.c
@@ -17,31 +17,6 @@
  */
 #include <ags/main.h>
 
-#include <ags-lib/object/ags_connectable.h>
-
-#include <ags/object/ags_main_loop.h>
-
-#include <ags/plugin/ags_ladspa_manager.h>
-
-#include <ags/file/ags_file.h>
-
-#include <ags/thread/ags_audio_loop.h>
-#include <ags/thread/ags_gui_thread.h>
-#include <ags/thread/ags_autosave_thread.h>
-#include <ags/thread/ags_single_thread.h>
-
-#include <ags/audio/ags_channel.h>
-#include <ags/audio/ags_output.h>
-#include <ags/audio/ags_input.h>
-#include <ags/audio/ags_recycling.h>
-
-#include <ags/X/machine/ags_matrix.h>
-#include <ags/X/machine/ags_synth.h>
-
-#include <ags/audio/ags_synths.h>
-
-#include <sys/mman.h>
-
 #include <gtk/gtk.h>
 #include <libintl.h>
 #include <stdio.h>
@@ -57,14 +32,7 @@
 #include <sys/types.h>
 #include <pwd.h>
 
-#include "config.h"
-
-void ags_main_class_init(AgsMainClass *ags_main);
-void ags_main_connectable_interface_init(AgsConnectableInterface *connectable);
-void ags_main_init(AgsMain *ags_main);
-void ags_main_connect(AgsConnectable *connectable);
-void ags_main_disconnect(AgsConnectable *connectable);
-void ags_main_finalize(GObject *gobject);
+#include <sys/mman.h>
 
 void ags_colors_alloc();
 
@@ -80,8 +48,6 @@ static const gchar *ags_config_devout = AGS_CONFIG_DEVOUT;
 extern void ags_thread_resume_handler(int sig);
 extern void ags_thread_suspend_handler(int sig);
 
-extern AgsConfig *config;
-
 extern GtkStyle *matrix_style;
 extern GtkStyle *ffplayer_style;
 extern GtkStyle *editor_style;
@@ -92,182 +58,6 @@ extern GtkStyle *note_edit_style;
 extern AgsLadspaManager *ags_ladspa_manager;
 
 struct sigaction ags_sigact;
-
-GType
-ags_main_get_type()
-{
-  static GType ags_type_main = 0;
-
-  if(!ags_type_main){
-    static const GTypeInfo ags_main_info = {
-      sizeof (AgsMainClass),
-      NULL, /* base_init */
-      NULL, /* base_finalize */
-      (GClassInitFunc) ags_main_class_init,
-      NULL, /* class_finalize */
-      NULL, /* class_data */
-      sizeof (AgsMain),
-      0,    /* n_preallocs */
-      (GInstanceInitFunc) ags_main_init,
-    };
-
-    static const GInterfaceInfo ags_connectable_interface_info = {
-      (GInterfaceInitFunc) ags_main_connectable_interface_init,
-      NULL, /* interface_finalize */
-      NULL, /* interface_data */
-    };
-
-    ags_type_main = g_type_register_static(G_TYPE_OBJECT,
-					   "AgsMain\0",
-					   &ags_main_info,
-					   0);
-
-    g_type_add_interface_static(ags_type_main,
-				AGS_TYPE_CONNECTABLE,
-				&ags_connectable_interface_info);
-  }
-
-  return (ags_type_main);
-}
-
-void
-ags_main_class_init(AgsMainClass *ags_main)
-{
-  GObjectClass *gobject;
-
-  ags_main_parent_class = g_type_class_peek_parent(ags_main);
-
-  /* GObjectClass */
-  gobject = (GObjectClass *) ags_main;
-
-  gobject->finalize = ags_main_finalize;
-}
-
-void
-ags_main_connectable_interface_init(AgsConnectableInterface *connectable)
-{
-  connectable->connect = ags_main_connect;
-  connectable->disconnect = ags_main_disconnect;
-}
-
-void
-ags_main_init(AgsMain *ags_main)
-{
-  GFile *file;
-  struct sigaction sa;
-  struct passwd *pw;
-  uid_t uid;
-  gchar *wdir, *filename;
-    
-  uid = getuid();
-  pw = getpwuid(uid);
-  
-  wdir = g_strdup_printf("%s/%s\0",
-			 pw->pw_dir,
-			 AGS_DEFAULT_DIRECTORY);
-
-  file = g_file_new_for_path(wdir);
-
-  g_file_make_directory(file,
-			NULL,
-			NULL);
-
-  ags_main->flags = 0;
-
-  ags_main->version = AGS_VERSION;
-  ags_main->build_id = AGS_BUILD_ID;
-
-  ags_main->log = (AgsLog *) g_object_new(AGS_TYPE_LOG,
-					  "file\0", stdout,
-					  NULL);
-  ags_colors_alloc();
-
-  ags_main->main_loop = NULL;
-
-  /* AgsAutosaveThread */
-  ags_main->autosave_thread = NULL;
-  ags_main->autosave_thread = ags_autosave_thread_new(NULL, ags_main);
-  g_object_ref(G_OBJECT(ags_main->autosave_thread));
-  ags_connectable_connect(AGS_CONNECTABLE(ags_main->autosave_thread));
-  
-  ags_main->thread_pool = ags_thread_pool_new(NULL);
-  ags_main->server = NULL;
-  ags_main->devout = NULL;
-  ags_main->window = NULL;
-  // ags_log_message(ags_default_log, "starting Advanced Gtk+ Sequencer\n\0");
-
-  sigfillset(&(ags_wait_mask));
-  sigdelset(&(ags_wait_mask), AGS_THREAD_SUSPEND_SIG);
-  sigdelset(&(ags_wait_mask), AGS_THREAD_RESUME_SIG);
-
-  sigfillset(&(sa.sa_mask));
-  sa.sa_flags = 0;
-
-  sa.sa_handler = ags_thread_resume_handler;
-  sigaction(AGS_THREAD_RESUME_SIG, &sa, NULL);
-
-  sa.sa_handler = ags_thread_suspend_handler;
-  sigaction(AGS_THREAD_SUSPEND_SIG, &sa, NULL);
-  
-  filename = g_strdup_printf("%s/%s\0",
-			     wdir,
-			     AGS_DEFAULT_CONFIG);
-
-  ags_main->config = config;
-  //TODO:JK: ugly
-  config->ags_main = ags_main;
-
-  g_free(filename);
-  g_free(wdir);
-}
-
-void
-ags_main_connect(AgsConnectable *connectable)
-{
-  AgsMain *ags_main;
-  GList *list;
-
-  ags_main = AGS_MAIN(connectable);
-
-  if((AGS_MAIN_CONNECTED & (ags_main->flags)) != 0)
-    return;
-
-  ags_main->flags |= AGS_MAIN_CONNECTED;
-
-  ags_connectable_connect(AGS_CONNECTABLE(ags_main->main_loop));
-  ags_connectable_connect(AGS_CONNECTABLE(ags_main->thread_pool));
-
-  g_message("connected threads\0");
-
-  list = ags_main->devout;
-
-  while(list != NULL){
-    ags_connectable_connect(AGS_CONNECTABLE(list->data));
-
-    list = list->next;
-  }
-
-  g_message("connected audio\0");
-
-  ags_connectable_connect(AGS_CONNECTABLE(ags_main->window));
-  g_message("connected gui\0");
-}
-
-void
-ags_main_disconnect(AgsConnectable *connectable)
-{
-  /* empty */
-}
-
-void
-ags_main_finalize(GObject *gobject)
-{
-  AgsMain *ags_main;
-
-  ags_main = AGS_MAIN(gobject);
-
-  G_OBJECT_CLASS(ags_main_parent_class)->finalize(gobject);
-}
 
 void
 ags_colors_alloc()
@@ -495,105 +285,6 @@ ags_colors_alloc()
 }
 
 void
-ags_main_load_config(AgsMain *ags_main)
-{
-  AgsConfig *config;
-  GList *list;
-
-  auto void ags_main_load_config_thread(AgsThread *thread);
-  auto void ags_main_load_config_devout(AgsDevout *devout);
-
-  void ags_main_load_config_thread(AgsThread *thread){
-    gchar *model;
-    
-    model = ags_config_get(config,
-			   ags_config_devout,
-			   "model\0");
-    
-    if(model != NULL){
-      if(!strncmp(model,
-		  "single-threaded\0",
-		  16)){
-	//TODO:JK: implement me
-	
-      }else if(!strncmp(model,
-			"multi-threaded",
-			15)){
-	//TODO:JK: implement me
-      }else if(!strncmp(model,
-			"super-threaded",
-			15)){
-	//TODO:JK: implement me
-      }
-    }
-  }
-  void ags_main_load_config_devout(AgsDevout *devout){
-    gchar *alsa_handle;
-    guint samplerate;
-    guint buffer_size;
-    guint pcm_channels, dsp_channels;
-
-    alsa_handle = ags_config_get(config,
-				 ags_config_devout,
-				 "alsa-handle\0");
-
-    dsp_channels = strtoul(ags_config_get(config,
-					  ags_config_devout,
-					  "dsp-channels\0"),
-			   NULL,
-			   10);
-    
-    pcm_channels = strtoul(ags_config_get(config,
-					  ags_config_devout,
-					  "pcm-channels\0"),
-			   NULL,
-			   10);
-
-    samplerate = strtoul(ags_config_get(config,
-					ags_config_devout,
-					"samplerate\0"),
-			 NULL,
-			 10);
-
-    buffer_size = strtoul(ags_config_get(config,
-					 ags_config_devout,
-					 "buffer-size\0"),
-			  NULL,
-			  10);
-    
-    g_object_set(G_OBJECT(devout),
-		 "device\0", alsa_handle,
-		 "dsp-channels\0", dsp_channels,
-		 "pcm-channels\0", pcm_channels,
-		 "frequency\0", samplerate,
-		 "buffer-size\0", buffer_size,
-		 NULL);
-  }
-  
-  if(ags_main == NULL){
-    return;
-  }
-
-  config = ags_main->config;
-
-  if(config == NULL){
-    return;
-  }
-
-  /* thread */
-  ags_main_load_config_thread(ags_main->main_loop);
-
-  /* devout */
-  list = ags_main->devout;
-
-  while(list != NULL){
-    ags_main_load_config_devout(AGS_DEVOUT(list->data));
-
-    list = list->next;
-  }
-}
-
-void
 ags_main_add_devout(AgsMain *ags_main,
 		    AgsDevout *devout)
 {
@@ -749,19 +440,21 @@ ags_signal_cleanup()
 int
 main(int argc, char **argv)
 {
-  AgsMain *ags_main;
-  AgsDevout *devout;
-  AgsWindow *window;
+  AgsApplicationContext *audio_application_context, *xorg_application_context;
   AgsGuiThread *gui_thread;
   GFile *autosave_file;
-  struct sched_param param;
-  struct rlimit rl;
+
   gchar *filename, *autosave_filename;
 
+  struct sched_param param;
+  struct rlimit rl;
+  struct sigaction sa;
+  
   struct passwd *pw;
   uid_t uid;
   gchar *wdir, *config_file;
   int result;
+  
   gboolean single_thread = FALSE;
   guint i;
 
@@ -799,6 +492,20 @@ main(int argc, char **argv)
   sigaction(SIGINT, &ags_sigact, (struct sigaction *) NULL);
   sigaction(SA_RESTART, &ags_sigact, (struct sigaction *) NULL);
 
+  /*  */
+  sigfillset(&(ags_wait_mask));
+  sigdelset(&(ags_wait_mask), AGS_THREAD_SUSPEND_SIG);
+  sigdelset(&(ags_wait_mask), AGS_THREAD_RESUME_SIG);
+
+  sigfillset(&(sa.sa_mask));
+  sa.sa_flags = 0;
+
+  sa.sa_handler = ags_thread_resume_handler;
+  sigaction(AGS_THREAD_RESUME_SIG, &sa, NULL);
+
+  sa.sa_handler = ags_thread_suspend_handler;
+  sigaction(AGS_THREAD_SUSPEND_SIG, &sa, NULL);
+
   /**/
   LIBXML_TEST_VERSION;
 
@@ -811,6 +518,8 @@ main(int argc, char **argv)
   ipatch_init();
 
   ao_initialize();
+
+  ags_colors_alloc();
 
   filename = NULL;
 
@@ -826,7 +535,7 @@ main(int argc, char **argv)
   config = ags_config_new();
   uid = getuid();
   pw = getpwuid(uid);
-  
+
   wdir = g_strdup_printf("%s/%s\0",
 			 pw->pw_dir,
 			 AGS_DEFAULT_DIRECTORY);
---
+++ src/ags/server/ags_server_application_context.c
@@ -0,0 +1,197 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2015 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <ags-lib/object/ags_connectable.h>
+
+void ags_server_application_context_class_init(AgsServerApplicationContextClass *server_application_context);
+void ags_server_application_context_connectable_interface_init(AgsConnectableInterface *connectable);
+void ags_server_application_context_init(AgsServerApplicationContext *server_application_context);
+void ags_server_application_context_set_property(GObject *gobject,
+						 guint prop_id,
+						 const GValue *value,
+						 GParamSpec *param_spec);
+void ags_server_application_context_get_property(GObject *gobject,
+						 guint prop_id,
+						 GValue *value,
+						 GParamSpec *param_spec);
+void ags_server_application_context_connect(AgsConnectable *connectable);
+void ags_server_application_context_disconnect(AgsConnectable *connectable);
+void ags_server_application_context_finalize(GObject *gobject);
+
+static AgsConnectableInterface* ags_server_application_context_parent_connectable_interface;
+
+GType
+ags_server_application_context_get_type()
+{
+  static GType ags_type_server_application_context = 0;
+
+  if(!ags_type_server_application_context){
+    static const GTypeInfo ags_server_application_context_info = {
+      sizeof (AgsServerApplicationContextClass),
+      NULL, /* base_init */
+      NULL, /* base_finalize */
+      (GClassInitFunc) ags_server_application_context_class_init,
+      NULL, /* class_finalize */
+      NULL, /* class_data */
+      sizeof (AgsServerApplicationContext),
+      0,    /* n_preallocs */
+      (GInstanceInitFunc) ags_server_application_context_init,
+    };
+
+    static const GInterfaceInfo ags_connectable_interface_info = {
+      (GInterfaceInitFunc) ags_server_application_context_connectable_interface_init,
+      NULL, /* interface_finalize */
+      NULL, /* interface_data */
+    };
+
+    ags_type_server_application_context = g_type_register_static(AGS_TYPE_APPLICATION_CONTEXT,
+								 "AgsServerApplicationContext\0",
+								 &ags_server_application_context_info,
+								 0);
+
+    g_type_add_interface_static(ags_type_server_application_context,
+				AGS_TYPE_CONNECTABLE,
+				&ags_connectable_interface_info);
+  }
+
+  return (ags_type_server_application_context);
+}
+
+void
+ags_server_application_context_class_init(AgsServerApplicationContextClass *server_application_context)
+{
+  GObjectClass *gobject;
+  AgsApplicationContextClass *application_context;
+
+  ags_server_application_context_parent_class = g_type_class_peek_parent(ags_server_application_context);
+
+  /* GObjectClass */
+  gobject = (GObjectClass *) ags_server_application_context;
+
+  gobject->set_property = ags_server_application_context_set_property;
+  gobject->get_property = ags_server_application_context_get_property;
+
+  gobject->finalize = ags_server_application_context_finalize;
+
+  /* AgsServerApplicationContextClass */
+  application_context = (AgsApplicationContextClass *) server_application_context_class;
+  
+  application_context->load_config = ags_server_application_context_load_config;
+  application_context->register_types = ags_server_application_context_register_types;
+}
+
+void
+ags_server_application_context_connectable_interface_init(AgsConnectableInterface *connectable)
+{
+  ags_server_application_context_parent_connectable_interface = g_type_interface_peek_parent(connectable);
+
+  connectable->connect = ags_server_application_context_connect;
+  connectable->disconnect = ags_server_application_context_disconnect;
+}
+
+void
+ags_server_application_context_init(AgsServerApplicationContext *server_application_context)
+{
+  server_application_context->flags = 0;
+}
+
+void
+ags_server_application_context_set_property(GObject *gobject,
+					    guint prop_id,
+					    const GValue *value,
+					    GParamSpec *param_spec)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  server_application_context = AGS_SERVER_APPLICATION_CONTEXT(gobject);
+
+  switch(prop_id){
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
+}
+
+void
+ags_server_application_context_get_property(GObject *gobject,
+					    guint prop_id,
+					    GValue *value,
+					    GParamSpec *param_spec)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  server_application_context = AGS_SERVER_APPLICATION_CONTEXT(gobject);
+
+  switch(prop_id){
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
+}
+
+void
+ags_server_application_context_connect(AgsConnectable *connectable)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  server_application_context = AGS_SERVER_APPLICATION_CONTEXT(connectable);
+
+  if((AGS_SERVER_APPLICATION_CONTEXT_CONNECTED & (server_application_context->flags)) != 0){
+    return;
+  }
+
+  ags_server_application_context_parent_connectable_interface->connect(connectable);
+}
+
+void
+ags_server_application_context_disconnect(AgsConnectable *connectable)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  server_application_context = AGS_SERVER_APPLICATION_CONTEXT(connectable);
+
+  if((AGS_SERVER_APPLICATION_CONTEXT_CONNECTED & (server_application_context->flags)) == 0){
+    return;
+  }
+
+  ags_server_application_context_parent_connectable_interface->disconnect(connectable);
+}
+
+void
+ags_server_application_context_finalize(GObject *gobject)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  G_OBJECT_CLASS(ags_server_application_context_parent_class)->finalize(gobject);
+
+  server_application_context = AGS_SERVER_APPLICATION_CONTEXT(gobject);
+}
+
+AgsServerApplicationContext*
+ags_server_application_context_new(AgsMainLoop *main_loop,
+				   AgsConfig *config)
+{
+  AgsServerApplicationContext *server_application_context;
+
+  server_application_context = (AgsServerApplicationContext *) g_object_new(AGS_TYPE_SERVER_APPLICATION_CONTEXT,
+									    "main-loop\0", main_loop,
+									    "config\0", config,
+									    NULL);
+
+  return(server_application_context);
+}
