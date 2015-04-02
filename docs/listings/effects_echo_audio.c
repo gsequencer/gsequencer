e7c6b12395c386a279f75a9685427d19d33e4c5a
Joël Krähemann <jkraehemann@gmail.com>
2015-04-02 20:35:14 +0000

    First pages commit

--- docs/listings/effects_echo_audio.c
+++
@@ -1,8 +0,0 @@
-AgsEchoAudio *echo_audio;
-
-echo_audio = (AgsEchoAudio *) g_object_new(AGS_TYPE_ECHO_AUDIO,
-					   "audio", audio,
-					   "container", echo_container,
-					   NULL);
-
-AGS_RECALL(echo_audio)->flags = AGS_RECALL_TEMPLATE;
--- docs/listings/linking_unsafe.c
+++
@@ -1,9 +0,0 @@
-/* link master_audio's input with slave_audio's output */
-ags_channel_set_link(ags_channel_nth(master_audio->input, 0),
-		     ags_channel_nth(slave_audio->output, 0),
-		     &error);
-  
-
-ags_channel_set_link(ags_channel_nth(master_audio->input, 1),
-		     ags_channel_nth(slave_audio->output, 1),
-		     &error);
