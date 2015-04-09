429737a6b7cd270f452c5560e5991d45b2c2da0c
Joël Krähemann <weedlight@gmail.com>
2015-04-09 17:30:57 +0000

    adding AgsMutexManager skelleton

---
+++ src/ags/thread/ags_mutex_manager.h
@@ -0,0 +1,62 @@
+/* AGS - Advanced GTK Sequencer
+ * Copyright (C) 2015 Joël Krähemann
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __AGS_MUTEX_MANAGER_H__
+#define __AGS_MUTEX_MANAGER_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+#define AGS_TYPE_MUTEX_MANAGER                (ags_mutex_manager_get_type())
+#define AGS_MUTEX_MANAGER(obj)                (G_TYPE_CHECK_INSTANCE_CAST((obj), AGS_TYPE_MUTEX_MANAGER, AgsMutexManager))
+#define AGS_MUTEX_MANAGER_CLASS(class)        (G_TYPE_CHECK_CLASS_CAST(class, AGS_TYPE_MUTEX_MANAGER, AgsMutexManagerClass))
+#define AGS_IS_MUTEX_MANAGER(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_MUTEX_MANAGER))
+#define AGS_IS_MUTEX_MANAGER_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_MUTEX_MANAGER))
+#define AGS_MUTEX_MANAGER_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS(obj, AGS_TYPE_MUTEX_MANAGER, AgsMutexManagerClass))
+
+typedef struct _AgsMutexManager AgsMutexManager;
+typedef struct _AgsMutexManagerClass AgsMutexManagerClass;
+typedef struct _AgsMutexEntry AgsMutexEntry;
+
+struct _AgsMutexManager
+{
+  GObject object;
+};
+
+struct _AgsMutexManagerClass
+{
+  GObjectClass object;
+
+  GList *entry;
+};
+
+struct _AgsMutexEntry
+{
+  GObject *lock_object;
+  pthread_mutex_t mutex;
+};
+
+GType ags_mutex_manager_get_type();
+
+AgsMutexEntry* ags_mutex_manager_find(AgsMutexManager *mutex_manager,
+				      GObject *lock_object);
+
+AgsMutexManager* ags_mutex_manager_get_instance();
+AgsMutexManager* ags_mutex_manager_new();
+
+#endif /*__AGS_MUTEX_MANAGER_H__*/
