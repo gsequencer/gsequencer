e32ee14f035fb17eae4f0f6506eab41493c1b9d8
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2015-06-24 19:05:37 +0200

    fixed copyright

--- src/ags/audio/recall/ags_copy_notation_audio.c
+++ src/ags/audio/recall/ags_copy_notation_audio.c
@@ -1,5 +1,8 @@
-/* This file is part of GSequencer.
- * 
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2015 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
  * GSequencer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
