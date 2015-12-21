cccddec45a05182a9e7a08884cbc262d251af0d0
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2015-12-21 01:54:52 +0100

    bug is reproduceable

--- src/ags/test/macro-exclusion-test.c
+++ src/ags/test/macro-exclusion-test.c
@@ -1,7 +1,24 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+#include <string.h>
+
+#include <math.h>
+
+#define MY_DATA_LENGTH (8192)
+#define MY_DUMMY_VAL0 (MY_DATA_LENGTH / 2)
+#define MY_DUMMY_VAL1 (0xff)
 
 static void my_static_dummy_function0();
 static void my_static_dummy_function1();
+
+struct _MyDummy{
+  unsigned long val0;
+  unsigned char val1;
+};
+
+double** my_data_function_v3f(int length);
+void my_dummy_init(struct _MyDummy *my_dummy);
 
 /* tests if global static memory is reachable within an exclusion macro */
 int do_global_static_memory_test();
@@ -20,21 +37,82 @@ int do_automatic_memory_test();
 
 /* declare global static */
 #ifdef MACRO_EXCLUSION_TEST_GLOBAL_STATIC_SWITCH
+#define MACRO_EXCLUSION_TEST_GLOBAL_STATIC_MY_STR0_LENGTH (22)
+
 static char *macro_exclusion_test_global_static_my_str0 = "default not reachable\0";
 static void (*macro_exclusion_test_global_static_my_func_ptr0) = &my_static_dummy_function0;
 #else
-static char *macro_exclusion_test_global_static_my_str = "default reachable\0";
-static void (*macro_exclusion_test_global_static_my_func_ptr) = &my_static_dummy_function1;
+#define MACRO_EXCLUSION_TEST_GLOBAL_STATIC_MY_STR1_LENGTH (18)
+
+static char *macro_exclusion_test_global_static_my_str1 = "default reachable\0";
+static void (*macro_exclusion_test_global_static_my_func_ptr1) = &my_static_dummy_function1;
 #endif
 
 /* declare global */
-#ifndef MACRO_EXCLUSION_TEST_GLOBAL_SWITCH
+#ifdef MACRO_EXCLUSION_TEST_GLOBAL_SWITCH
+#define MACRO_EXCLUSION_TEST_GLOBAL_MY_STR0_LENGTH (22)
+
 char *macro_exclusion_test_global_my_str0 = "default not reachable\0";
 void (*macro_exclusion_test_global_my_func_ptr0) = &my_static_dummy_function0;
 #else
+#define MACRO_EXCLUSION_TEST_GLOBAL_MY_STR1_LENGTH (18)
+
 char *macro_exclusion_test_global_my_str1 = "default reachable\0";
 void (*macro_exclusion_test_global_my_func_ptr1) = &my_static_dummy_function1;
 #endif
+
+double **my_complex_i = NULL;
+
+/* generate some data */
+double**
+my_data_function_v3f(int length)
+{
+  double **plot_v3f;
+
+  double value;
+
+  unsigned int i, i_stop;
+  unsigned int j, j_stop;
+
+  /* allocate vector array */
+  plot_v3f = (double**) malloc(length * sizeof(double*));
+  
+  /* do a plot */
+  i_stop =
+    j_stop = length / 2;
+
+  /* generate a sin curve quarter through -1|1 */
+  for(i = 0; i < i_stop; i++){
+    /* allocate vector */
+    plot_v3f[i] = (double *) malloc(3 * sizeof(double));
+
+    /* calculate value and fill */
+    value = -1.0 * sin((i_stop) * (2.0 * M_PI) / ((double) i) / 4.0);
+
+    plot_v3f[i][0] = -1.0 * value;
+    plot_v3f[i][1] = value;
+    plot_v3f[i][2] = value;
+  }
+
+  /* generate exponential curve */
+  for(j = 0; j < j_stop; i++, j++){
+    /* allocate vector */
+    plot_v3f[i] = (double *) malloc(3 * sizeof(double));
+    
+    /* calculate value and fill */
+    plot_v3f[i][0] = -1.0 * exp((1.0 / 3.0) * log((double) j));
+    plot_v3f[i][1] = exp(3.0 * log((double) j));
+    plot_v3f[i][2] = (double) j;
+  }
+
+  /* return allocated and filled buffer */
+  return(plot_v3f);
+}
+
+void
+my_dummy_init(struct _MyDummy *my_dummy)
+{
+}
 
 /* just for checking address */
 static void
@@ -64,10 +142,41 @@ my_static_dummy_function1()
 int
 do_global_static_memory_test()
 {
+  /* declare some variables */
 #ifdef MACRO_EXCLUSION_TEST_GLOBAL_STATIC_SWITCH
-  //TODO:JK: implement me
+  char *test_str0 = macro_exclusion_test_global_static_my_str0;
+  void (*test_func_ptr0) = macro_exclusion_test_global_static_my_func_ptr0;
 #else
-  //TODO:JK: implement me
+  char *test_str1 = macro_exclusion_test_global_static_my_str1;
+  void (*test_func_ptr1) = macro_exclusion_test_global_static_my_func_ptr1;
+#endif
+
+#ifdef MACRO_EXCLUSION_TEST_GLOBAL_STATIC_SWITCH
+  /* check reachable */
+  if(test_str0 != macro_exclusion_test_global_static_my_str0){
+    printf("ERROR\n\0");
+    return(-1);
+  }
+
+  if(strncmp(test_str0,
+	     macro_exclusion_test_global_static_my_str0,
+	     MACRO_EXCLUSION_TEST_GLOBAL_STATIC_MY_STR0_LENGTH)){
+    printf("ERROR\n\0");
+    return(-1);
+  }
+#else
+  /* check reachable */
+  if(test_str1 != macro_exclusion_test_global_static_my_str1){
+    printf("ERROR\n\0");
+    return(-1);
+  }
+
+  if(strncmp(test_str1,
+	     macro_exclusion_test_global_static_my_str1,
+	     MACRO_EXCLUSION_TEST_GLOBAL_STATIC_MY_STR1_LENGTH)){
+    printf("ERROR\n\0");
+    return(-1);
+  }
 #endif
   
   return(0);
@@ -77,12 +186,32 @@ do_global_static_memory_test()
 int
 do_global_memory_test()
 {
+  if(my_complex_i[MY_DATA_LENGTH - 1][1] > 1.0){ /* y is definitely bigger if not MY_DATA_LENGTH < 2 */
 #ifdef MACRO_EXCLUSION_TEST_GLOBAL_SWITCH
-  //TODO:JK: implement me
+    printf("don't define MACRO_EXCLUSION_TEST_GLOBAL_SWITCH\n\0");
+
+    return(-1);
 #else
-  //TODO:JK: implement me
-#endif
+    char *test_str1 = macro_exclusion_test_global_my_str1;
+    void (*test_func_ptr1) = macro_exclusion_test_global_my_func_ptr1;
+
+    printf("global memory test\n\0");
   
+    /* check reachable */
+    if(test_str1 != macro_exclusion_test_global_my_str1){
+      printf("ERROR\n\0");
+      return(-1);
+    }
+
+    if(strncmp(test_str1,
+	       macro_exclusion_test_global_my_str1,
+	       MACRO_EXCLUSION_TEST_GLOBAL_MY_STR1_LENGTH)){
+      printf("ERROR\n\0");
+      return(-1);
+    }
+#endif
+  }
+
   return(0);
 }
 
@@ -90,11 +219,15 @@ do_global_memory_test()
 int
 do_static_memory_test()
 {
+  if(my_complex_i[MY_DATA_LENGTH - 1][1] > 1.0){ /* y is definitely bigger if not MY_DATA_LENGTH < 2 */
 #ifdef MACRO_EXCLUSION_TEST_STATIC_SWITCH
-  //TODO:JK: implement me
+    printf("don't define MACRO_EXCLUSION_TEST_STATIC_SWITCH\n\0");
+
+    return(-1);
 #else
   //TODO:JK: implement me
 #endif
+  }
   
   return(0);
 }
@@ -104,11 +237,25 @@ int
 do_local_memory_test()
 {
 #ifdef MACRO_EXCLUSION_TEST_LOCAL_SWITCH
-  //TODO:JK: implement me
+    printf("don't define MACRO_EXCLUSION_TEST_LOCAL_SWITCH\n\0");
+
+    return(-1);
 #else
-  //TODO:JK: implement me
+    struct _MyDummy my_dummy;
+
+    my_dummy_init(&my_dummy);
+
+    if(my_dummy.val0 != MY_DUMMY_VAL0){
+      printf("ERROR\n\0");
+      return(-1);
+    }
+
+    if(my_dummy.val1 != MY_DUMMY_VAL1){
+      printf("ERROR\n\0");
+      return(-1);
+    }
 #endif
-  
+
   return(0);
 }
 
@@ -117,11 +264,19 @@ int
 do_automatic_memory_test()
 {
 #ifdef MACRO_EXCLUSION_TEST_AUTOMATIC_SWITCH
+    printf("don't define MACRO_EXCLUSION_TEST_AUTOMATIC_SWITCH\n\0");
+
+    return(-1);
+#else
+  //TODO:JK: implement me
+#endif
+
+#ifdef MACRO_EXCLUSION_TEST_AUTOMATIC_SWITCH
   //TODO:JK: implement me
 #else
   //TODO:JK: implement me
 #endif
-  
+
   return(0);
 }
 
@@ -134,6 +289,9 @@ main(int argc, char **argv)
 
   failed_tests = 0;
 
+  /* initialize data */
+  my_complex_i = my_data_function_v3f(MY_DATA_LENGTH);
+  
   /* test #1 */
   retval = do_global_static_memory_test();
 
