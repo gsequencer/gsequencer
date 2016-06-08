9b80efb356a5609ded7be185ae9239ca0781516a
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2016-06-08 00:28:47 +0000

    added ags_simple_file.dtd

--- ags/util/ags_id_generator.c
+++ ags/util/ags_id_generator.c
@@ -50,8 +50,10 @@ ags_id_generator_create_uuid()
   gchar *uuid_str;
 
   /* make sure uuid is really unique */
-  while(uuid_generate_time_safe(out) != 0);
-
+  //FIXME:JK: uncomment me
+  //  while(uuid_generate_time_safe(out) != 0);
+  uuid_generate_time_safe(out);
+  
   /* create a string representation */
   uuid_str = (gchar *) malloc((AGS_ID_GENERATOR_DEFAULT_UUID_LENGTH + 1) * sizeof(gchar));
   uuid_unparse(out, uuid_str);
