b2cd0d6ba353161a5b373cca74cd8c0796fa420d
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2016-11-08 21:59:06 +0100

    implemented properties of AgsAddEffect

--- ags/audio/task/ags_add_effect.c
+++ ags/audio/task/ags_add_effect.c
@@ -24,11 +24,26 @@
 void ags_add_effect_class_init(AgsAddEffectClass *add_effect);
 void ags_add_effect_connectable_interface_init(AgsConnectableInterface *connectable);
 void ags_add_effect_init(AgsAddEffect *add_effect);
+void ags_add_effect_set_property(GObject *gobject,
+				 guint prop_id,
+				 const GValue *value,
+				 GParamSpec *param_spec);
+void ags_add_effect_get_property(GObject *gobject,
+				 guint prop_id,
+				 GValue *value,
+				 GParamSpec *param_spec);
 void ags_add_effect_connect(AgsConnectable *connectable);
 void ags_add_effect_disconnect(AgsConnectable *connectable);
 void ags_add_effect_finalize(GObject *gobject);
 
 void ags_add_effect_launch(AgsTask *task);
+
+enum{
+  PROP_0,
+  PROP_CHANNEL,
+  PROP_FILENAME,
+  PROP_EFFECT,
+};
 
 /**
  * SECTION:ags_add_effect
@@ -85,13 +100,65 @@ ags_add_effect_class_init(AgsAddEffectClass *add_effect)
 {
   GObjectClass *gobject;
   AgsTaskClass *task;
-
+  GParamSpec *param_spec;
+  
   ags_add_effect_parent_class = g_type_class_peek_parent(add_effect);
 
   /* gobject */
   gobject = (GObjectClass *) add_effect;
 
+  gobject->set_property = ags_add_effect_set_property;
+  gobject->get_property = ags_add_effect_get_property;
+
   gobject->finalize = ags_add_effect_finalize;
+
+  /**
+   * AgsAddEffect:channel:
+   *
+   * The assigned #AgsChannel
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_object("channel\0",
+				   "channel of add effect\0",
+				   "The channel of add effect task\0",
+				   AGS_TYPE_CHANNEL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_CHANNEL,
+				  param_spec);
+
+  /**
+   * AgsAddEffect:filename:
+   *
+   * The assigned filename.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("filename\0",
+				   "the filename\0",
+				   "The filename containing the effect0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_FILENAME,
+				  param_spec);
+
+  /**
+   * AgsAddEffect:effect:
+   *
+   * The assigned effect.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("effect\0",
+				   "the effect\0",
+				   "The effect",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_EFFECT,
+				  param_spec);
 
   /* task */
   task = (AgsTaskClass *) add_effect;
@@ -114,6 +181,110 @@ ags_add_effect_init(AgsAddEffect *add_effect)
   add_effect->channel = NULL;
   add_effect->filename = NULL;
   add_effect->effect = NULL;
+}
+
+void
+ags_add_effect_set_property(GObject *gobject,
+			    guint prop_id,
+			    const GValue *value,
+			    GParamSpec *param_spec)
+{
+  AgsAddEffect *add_effect;
+
+  add_effect = AGS_ADD_EFFECT(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      GObject *channel;
+
+      channel = (GObject *) g_value_get_object(value);
+
+      if(add_effect->channel == (GObject *) channel){
+	return;
+      }
+
+      if(add_effect->channel != NULL){
+	g_object_unref(add_effect->channel);
+      }
+
+      if(channel != NULL){
+	g_object_ref(channel);
+      }
+
+      add_effect->channel = (GObject *) channel;
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      gchar *filename;
+
+      filename = g_value_get_string(value);
+
+      if(add_effect->filename == filename){
+	return;
+      }
+
+      if(add_effect->filename != NULL){
+        g_free(add_effect->filename);
+      }
+
+      add_effect->filename = g_strdup(filename);
+    }
+    break;
+  case PROP_EFFECT:
+    {
+      gchar *effect;
+
+      effect = g_value_get_string(value);
+
+      if(add_effect->effect == effect){
+	return;
+      }
+
+      if(add_effect->effect != NULL){
+        g_free(add_effect->effect);
+      }
+
+      add_effect->effect = g_strdup(effect);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
+}
+
+void
+ags_add_effect_get_property(GObject *gobject,
+			    guint prop_id,
+			    GValue *value,
+			    GParamSpec *param_spec)
+{
+  AgsAddEffect *add_effect;
+
+  add_effect = AGS_ADD_EFFECT(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      g_value_set_object(value, add_effect->channel);
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      g_value_set_string(value, add_effect->filename);
+    }
+    break;
+  case PROP_EFFECT:
+    {
+      g_value_set_string(value, add_effect->effect);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
 }
 
 void
