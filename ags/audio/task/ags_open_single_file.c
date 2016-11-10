69af13824aacd80f26545ec6962f6e68dd3cafdb
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2016-11-10 10:19:25 +0100

    added properties to AgsOpenSingleFile

--- ags/audio/task/ags_open_single_file.c
+++ ags/audio/task/ags_open_single_file.c
@@ -32,13 +32,40 @@
 void ags_open_single_file_class_init(AgsOpenSingleFileClass *open_single_file);
 void ags_open_single_file_connectable_interface_init(AgsConnectableInterface *connectable);
 void ags_open_single_file_init(AgsOpenSingleFile *open_single_file);
+void ags_open_single_file_set_property(GObject *gobject,
+				       guint prop_id,
+				       const GValue *value,
+				       GParamSpec *param_spec);
+void ags_open_single_file_get_property(GObject *gobject,
+				       guint prop_id,
+				       GValue *value,
+				       GParamSpec *param_spec);
 void ags_open_single_file_connect(AgsConnectable *connectable);
 void ags_open_single_file_disconnect(AgsConnectable *connectable);
 void ags_open_single_file_finalize(GObject *gobject);
 void ags_open_single_file_launch(AgsTask *task);
 
+/**
+ * SECTION:ags_open_single_file
+ * @short_description: open single file task
+ * @title: AgsOpenSingleFile
+ * @section_id:
+ * @include: ags/audio/task/ags_open_single_file.h
+ *
+ * The #AgsOpenFile task opens one single file.
+ */
+
 static gpointer ags_open_single_file_parent_class = NULL;
 static AgsConnectableInterface *ags_open_single_file_parent_connectable_interface;
+
+enum{
+  PROP_0,
+  PROP_CHANNEL,
+  PROP_SOUNDCARD,
+  PROP_FILENAME,
+  PROP_START_CHANNEL,
+  PROP_AUDIO_CHANNELS,
+};
 
 GType
 ags_open_single_file_get_type()
@@ -82,13 +109,102 @@ ags_open_single_file_class_init(AgsOpenSingleFileClass *open_single_file)
 {
   GObjectClass *gobject;
   AgsTaskClass *task;
+  GParamSpec *param_spec;
 
   ags_open_single_file_parent_class = g_type_class_peek_parent(open_single_file);
 
   /* GObject */
   gobject = (GObjectClass *) open_single_file;
 
+  gobject->set_property = ags_open_single_file_set_property;
+  gobject->get_property = ags_open_single_file_get_property;
+
   gobject->finalize = ags_open_single_file_finalize;
+
+  /* properties */
+  /**
+   * AgsOpenSingleFile:channel:
+   *
+   * The assigned #AgsChannel
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_object("channel\0",
+				   "channel of open file\0",
+				   "The channel of open file task\0",
+				   AGS_TYPE_CHANNEL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_CHANNEL,
+				  param_spec);
+
+  /**
+   * AgsOpenSingleFile:soundcard:
+   *
+   * The assigned #AgsSoundcard
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_object("soundcard\0",
+				   "soundcard of open file\0",
+				   "The soundcard of open file task\0",
+				   G_TYPE_OBJECT,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_SOUNDCARD,
+				  param_spec);
+
+  /**
+   * AgsOpenSingleFile:filename:
+   *
+   * The assigned filename.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("filename\0",
+				   "the filename\0",
+				   "The filename containing the output\0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_FILENAME,
+				  param_spec);
+
+  /**
+   * AgsOpenSingleFile:start-channel:
+   *
+   * The start channel of within pad @channel.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_uint("start-channel\0",
+				 "start channel\0",
+				 "The start channel\0",
+				 0,
+				 G_MAXUINT,
+				 0,
+				 G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_START_CHANNEL,
+				  param_spec);
+
+  /**
+   * AgsOpenSingleFile:audio-channels:
+   *
+   * The audio channels count to read.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_uint("audio-channels\0",
+				 "audio channels\0",
+				 "The audio channels to read\0",
+				 0,
+				 G_MAXUINT,
+				 0,
+				 G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_AUDIO_CHANNELS,
+				  param_spec);
 
   /* AgsTask */
   task = (AgsTaskClass *) open_single_file;
@@ -113,6 +229,134 @@ ags_open_single_file_init(AgsOpenSingleFile *open_single_file)
   open_single_file->filename = NULL;
   open_single_file->start_channel = 0;
   open_single_file->audio_channels = 0;
+}
+
+void
+ags_open_single_file_set_property(GObject *gobject,
+			       guint prop_id,
+			       const GValue *value,
+			       GParamSpec *param_spec)
+{
+  AgsOpenSingleFile *open_single_file;
+
+  open_single_file = AGS_OPEN_SINGLE_FILE(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      AgsChannel *channel;
+
+      channel = (AgsChannel *) g_value_get_object(value);
+
+      if(open_single_file->channel == (GObject *) channel){
+	return;
+      }
+
+      if(open_single_file->channel != NULL){
+	g_object_unref(open_single_file->channel);
+      }
+
+      if(channel != NULL){
+	g_object_ref(channel);
+      }
+
+      open_single_file->channel = (GObject *) channel;
+    }
+    break;
+  case PROP_SOUNDCARD:
+    {
+      GObject *soundcard;
+
+      soundcard = (GObject *) g_value_get_object(value);
+
+      if(open_single_file->soundcard == (GObject *) soundcard){
+	return;
+      }
+
+      if(open_single_file->soundcard != NULL){
+	g_object_unref(open_single_file->soundcard);
+      }
+
+      if(soundcard != NULL){
+	g_object_ref(soundcard);
+      }
+
+      open_single_file->soundcard = (GObject *) soundcard;
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      gchar *filename;
+
+      filename = g_value_get_string(value);
+
+      if(open_single_file->filename == filename){
+	return;
+      }
+
+      if(open_single_file->filename != NULL){
+        g_free(open_single_file->filename);
+      }
+
+      open_single_file->filename = g_strdup(filename);
+    }
+    break;
+  case PROP_START_CHANNEL:
+    {
+      open_single_file->start_channel = g_value_get_uint(value);
+    }
+    break;
+  case PROP_AUDIO_CHANNELS:
+    {
+      open_single_file->audio_channels = g_value_get_uint(value);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
+}
+
+void
+ags_open_single_file_get_property(GObject *gobject,
+			       guint prop_id,
+			       GValue *value,
+			       GParamSpec *param_spec)
+{
+  AgsOpenSingleFile *open_single_file;
+
+  open_single_file = AGS_OPEN_SINGLE_FILE(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      g_value_set_object(value, open_single_file->channel);
+    }
+    break;
+  case PROP_SOUNDCARD:
+    {
+      g_value_set_object(value, open_single_file->soundcard);
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      g_value_set_string(value, open_single_file->filename);
+    }
+    break;
+  case PROP_START_CHANNEL:
+    {
+      g_value_set_uint(value, open_single_file->start_channel);
+    }
+    break;
+  case PROP_AUDIO_CHANNELS:
+    {
+      g_value_set_uint(value, open_single_file->audio_channels);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
 }
 
 void
