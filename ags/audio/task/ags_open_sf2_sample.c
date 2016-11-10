dc1599e397fc46fc9666609d6fc40ec8fa6cc300
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2016-11-10 09:51:59 +0100

    added properties to AgsOpenFile and AgsOpenSf2Sample

--- ags/audio/task/ags_open_sf2_sample.c
+++ ags/audio/task/ags_open_sf2_sample.c
@@ -33,6 +33,14 @@
 void ags_open_sf2_sample_class_init(AgsOpenSf2SampleClass *open_sf2_sample);
 void ags_open_sf2_sample_connectable_interface_init(AgsConnectableInterface *connectable);
 void ags_open_sf2_sample_init(AgsOpenSf2Sample *open_sf2_sample);
+void ags_open_sf2_sample_set_property(GObject *gobject,
+				      guint prop_id,
+				      const GValue *value,
+				      GParamSpec *param_spec);
+void ags_open_sf2_sample_get_property(GObject *gobject,
+				      guint prop_id,
+				      GValue *value,
+				      GParamSpec *param_spec);
 void ags_open_sf2_sample_connect(AgsConnectable *connectable);
 void ags_open_sf2_sample_disconnect(AgsConnectable *connectable);
 void ags_open_sf2_sample_finalize(GObject *gobject);
@@ -50,6 +58,15 @@ void ags_open_sf2_sample_launch(AgsTask *task);
 
 static gpointer ags_open_sf2_sample_parent_class = NULL;
 static AgsConnectableInterface *ags_open_sf2_sample_parent_connectable_interface;
+
+enum{
+  PROP_0,
+  PROP_CHANNEL,
+  PROP_FILENAME,
+  PROP_PRESET,
+  PROP_INSTRUMENT,
+  PROP_SAMPLE,
+};
 
 GType
 ags_open_sf2_sample_get_type()
@@ -93,13 +110,98 @@ ags_open_sf2_sample_class_init(AgsOpenSf2SampleClass *open_sf2_sample)
 {
   GObjectClass *gobject;
   AgsTaskClass *task;
-
+  GParamSpec *param_spec;
+  
   ags_open_sf2_sample_parent_class = g_type_class_peek_parent(open_sf2_sample);
 
   /* GObject */
   gobject = (GObjectClass *) open_sf2_sample;
 
+  gobject->set_property = ags_open_sf2_sample_set_property;
+  gobject->get_property = ags_open_sf2_sample_get_property;
+
   gobject->finalize = ags_open_sf2_sample_finalize;
+
+  /* properties */
+  /**
+   * AgsOpenSf2Sample:channel:
+   *
+   * The assigned #AgsChannel
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_object("channel\0",
+				   "channel of open sf2 sample\0",
+				   "The channel of open sf2 sample task\0",
+				   AGS_TYPE_CHANNEL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_CHANNEL,
+				  param_spec);
+  
+  /**
+   * AgsOpenSf2Sample:filename:
+   *
+   * The assigned filename.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("filename\0",
+				   "the filename\0",
+				   "The filename containing the sf2 sample\0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_FILENAME,
+				  param_spec);
+
+  /**
+   * AgsOpenSf2Sample:preset:
+   *
+   * The assigned preset.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("preset\0",
+				   "the preset\0",
+				   "The preset containing the sf2 sample\0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_PRESET,
+				  param_spec);
+
+  /**
+   * AgsOpenSf2Sample:instrument:
+   *
+   * The assigned instrument.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("instrument\0",
+				   "the instrument\0",
+				   "The instrument containing the sf2 sample\0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_INSTRUMENT,
+				  param_spec);
+
+  /**
+   * AgsOpenSf2Sample:sample:
+   *
+   * The assigned sample.
+   * 
+   * Since: 1.0.0
+   */
+  param_spec = g_param_spec_string("sample\0",
+				   "the sample\0",
+				   "The sf2 sample\0",
+				   NULL,
+				   G_PARAM_READABLE | G_PARAM_WRITABLE);
+  g_object_class_install_property(gobject,
+				  PROP_FILENAME,
+				  param_spec);
 
   /* AgsTask */
   task = (AgsTaskClass *) open_sf2_sample;
@@ -124,6 +226,154 @@ ags_open_sf2_sample_init(AgsOpenSf2Sample *open_sf2_sample)
   open_sf2_sample->preset = NULL;
   open_sf2_sample->instrument = NULL;
   open_sf2_sample->sample = NULL;
+}
+
+void
+ags_open_sf2_sample_set_property(GObject *gobject,
+			       guint prop_id,
+			       const GValue *value,
+			       GParamSpec *param_spec)
+{
+  AgsOpenSf2Sample *open_sf2_sample;
+
+  open_sf2_sample = AGS_OPEN_SF2_SAMPLE(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      AgsChannel *channel;
+
+      channel = (AgsChannel *) g_value_get_object(value);
+
+      if(open_sf2_sample->channel == (GObject *) channel){
+	return;
+      }
+
+      if(open_sf2_sample->channel != NULL){
+	g_object_unref(open_sf2_sample->channel);
+      }
+
+      if(channel != NULL){
+	g_object_ref(channel);
+      }
+
+      open_sf2_sample->channel = (GObject *) channel;
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      gchar *filename;
+
+      filename = g_value_get_string(value);
+
+      if(open_sf2_sample->filename == filename){
+	return;
+      }
+
+      if(open_sf2_sample->filename != NULL){
+        g_free(open_sf2_sample->filename);
+      }
+
+      open_sf2_sample->filename = g_strdup(filename);
+    }
+    break;
+  case PROP_PRESET:
+    {
+      gchar *preset;
+
+      preset = g_value_get_string(value);
+
+      if(open_sf2_sample->preset == preset){
+	return;
+      }
+
+      if(open_sf2_sample->preset != NULL){
+        g_free(open_sf2_sample->preset);
+      }
+
+      open_sf2_sample->preset = g_strdup(preset);
+    }
+    break;
+  case PROP_INSTRUMENT:
+    {
+      gchar *instrument;
+
+      instrument = g_value_get_string(value);
+
+      if(open_sf2_sample->instrument == instrument){
+	return;
+      }
+
+      if(open_sf2_sample->instrument != NULL){
+        g_free(open_sf2_sample->instrument);
+      }
+
+      open_sf2_sample->instrument = g_strdup(instrument);
+    }
+    break;
+  case PROP_SAMPLE:
+    {
+      gchar *sample;
+
+      sample = g_value_get_string(value);
+
+      if(open_sf2_sample->sample == sample){
+	return;
+      }
+
+      if(open_sf2_sample->sample != NULL){
+        g_free(open_sf2_sample->sample);
+      }
+
+      open_sf2_sample->sample = g_strdup(sample);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
+}
+
+void
+ags_open_sf2_sample_get_property(GObject *gobject,
+				 guint prop_id,
+				 GValue *value,
+				 GParamSpec *param_spec)
+{
+  AgsOpenSf2Sample *open_sf2_sample;
+
+  open_sf2_sample = AGS_OPEN_SF2_SAMPLE(gobject);
+
+  switch(prop_id){
+  case PROP_CHANNEL:
+    {
+      g_value_set_object(value, open_sf2_sample->channel);
+    }
+    break;
+  case PROP_FILENAME:
+    {
+      g_value_set_string(value, open_sf2_sample->filename);
+    }
+    break;
+  case PROP_PRESET:
+    {
+      g_value_set_string(value, open_sf2_sample->preset);
+    }
+    break;
+  case PROP_INSTRUMENT:
+    {
+      g_value_set_string(value, open_sf2_sample->instrument);
+    }
+    break;
+  case PROP_SAMPLE:
+    {
+      g_value_set_string(value, open_sf2_sample->sample);
+    }
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID(gobject, prop_id, param_spec);
+    break;
+  }
 }
 
 void
