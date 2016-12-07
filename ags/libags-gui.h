f1431b3179693f328b7065db252b5d11f7ffb62b
Joël Krähemann <jkraehemann@gmail.com>
2016-12-07 19:28:42 +0100

    provide headers libags.h, libags-audio.h and libags-gui.h

---
+++ ags/libags-gui.h
@@ -0,0 +1,33 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2015 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __LIBAGS_GUI_H__
+#define __LIBAGS_GUI_H__
+
+#include <ags/widget/ags_cartesian.h>
+#include <ags/widget/ags_dial.h>
+#include <ags/widget/ags_expander_set.h>
+#include <ags/widget/ags_hindicator.h>
+#include <ags/widget/ags_indicator.h>
+#include <ags/widget/ags_led.h>
+#include <ags/widget/ags_ruler.h>
+#include <ags/widget/ags_vindicator.h>
+#include <ags/widget/ags_container.h>
+
+#endif /*__LIBAGS_GUI_H__*/
