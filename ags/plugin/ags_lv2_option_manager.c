f9014cd47ba86347f5d700b5f4362211ce37f76b
Joël Krähemann <jkraehemann-guest@users.alioth.debian.org>
2017-01-14 13:31:17 +0100

    fixed compile errors

--- ags/plugin/ags_lv2_option_manager.c
+++ ags/plugin/ags_lv2_option_manager.c
@@ -110,6 +110,16 @@ ags_lv2_option_manager_class_init(AgsLv2OptionManagerClass *lv2_option_manager)
   gobject = (GObjectClass *) lv2_option_manager;
 
   gobject->finalize = ags_lv2_option_manager_finalize;
+
+  /* AgsLv2OptionManager  */
+  lv2_option_manager->get_option = ags_lv2_option_manager_get_option;
+  lv2_option_manager->set_option = ags_lv2_option_manager_set_option;
+}
+
+GQuark
+ags_lv2_option_manager_error_quark()
+{
+  return(g_quark_from_static_string("ags-lv2-option-manager-error-quark\0"));
 }
 
 void
@@ -158,27 +168,27 @@ ags_lv2_option_manager_destroy_data(gpointer data)
 /**
  * ags_lv2_option_manager_insert:
  * @lv2_option_manager: the #AgsLv2OptionManager
- * @option_ressource: the #AgsOptionRessource-struct as key
+ * @lv2_option_ressource: the #AgsOptionRessource-struct as key
  * @data: the data
  *
- * Inserts a data into hash associated with @option_ressource.
+ * Inserts a data into hash associated with @lv2_option_ressource.
  * 
  * Returns: %TRUE on success, otherwise %FALSE
  *
  * Since: 0.7.128
  */
 gboolean
-ags_lv2_option_manager_insert(AgsLv2OptionManager *option_manager,
-			      AgsLv2OptionRessource *option_ressource, gpointer data)
+ags_lv2_option_manager_insert(AgsLv2OptionManager *lv2_option_manager,
+			      AgsLv2OptionRessource *lv2_option_ressource, gpointer data)
 {
   if(lv2_option_manager == NULL ||
-     option_ressource == NULL ||
+     lv2_option_ressource == NULL ||
      data == NULL){
     return(FALSE);
   }
 
   g_hash_table_insert(lv2_option_manager->ressource,
-		      option_ressource, data);
+		      lv2_option_ressource, data);
 
   return(TRUE);
 }
@@ -186,9 +196,9 @@ ags_lv2_option_manager_insert(AgsLv2OptionManager *option_manager,
 /**
  * ags_lv2_option_manager_remove:
  * @lv2_option_manager: the #AgsLv2OptionManager
- * @option_ressource: the struct to remove
+ * @lv2_option_ressource: the struct to remove
  * 
- * Removes an entry associated with @option_ressource.
+ * Removes an entry associated with @lv2_option_ressource.
  *
  * Returns: %TRUE as successfully removed, otherwise %FALSE
  *
@@ -196,12 +206,12 @@ ags_lv2_option_manager_insert(AgsLv2OptionManager *option_manager,
  */
 gboolean
 ags_lv2_option_manager_remove(AgsLv2OptionManager *lv2_option_manager,
-			      AgsLv2OptionRessource *option_ressource)
+			      AgsLv2OptionRessource *lv2_option_ressource)
 {
   gpointer data;
 
   data = g_hash_table_lookup(lv2_option_manager->ressource,
-			     option_ressource);
+			     lv2_option_ressource);
 
   if(data != NULL){
     g_hash_table_remove(lv2_option_manager->ressource,
@@ -214,9 +224,9 @@ ags_lv2_option_manager_remove(AgsLv2OptionManager *lv2_option_manager,
 /**
  * ags_lv2_option_manager_lookup:
  * @lv2_option_manager: the #AgsLv2OptionManager
- * @option_ressource: the #AgsLv2OptionRessource to lookup
+ * @lv2_option_ressource: the #AgsLv2OptionRessource to lookup
  *
- * Lookup a ressource associated with @option_ressource in
+ * Lookup a ressource associated with @lv2_option_ressource in
  * @lv2_option_manager.
  *
  * Returns: the pointer on success, else NULL
@@ -225,18 +235,18 @@ ags_lv2_option_manager_remove(AgsLv2OptionManager *lv2_option_manager,
  */
 gpointer
 ags_lv2_option_manager_lookup(AgsLv2OptionManager *lv2_option_manager,
-			      AgsLv2OptionRessource *option_ressource)
+			      AgsLv2OptionRessource *lv2_option_ressource)
 {
   gpointer data;
   
-  value = (GValue *) g_hash_table_lookup(lv2_option_manager->ressource,
-					 uri);
-
-  return(value);
+  data = (gpointer) g_hash_table_lookup(lv2_option_manager->ressource,
+					lv2_option_ressource);
+  
+  return(data);
 }
 
 void
-ags_lv2_option_manager_real_get_option(AgsLv2OptionManager *option_manager,
+ags_lv2_option_manager_real_get_option(AgsLv2OptionManager *lv2_option_manager,
 				       LV2_Handle instance,
 				       LV2_Options_Option* options,
 				       GError **error)
@@ -246,7 +256,7 @@ ags_lv2_option_manager_real_get_option(AgsLv2OptionManager *option_manager,
 
 /**
  * ags_lv2_option_manager_get_option:
- * @option_manager: the #AgsLv2OptionManager
+ * @lv2_option_manager: the #AgsLv2OptionManager
  * @instance: the instance
  * @options: the options
  * @error: the #GError-struct
@@ -256,7 +266,7 @@ ags_lv2_option_manager_real_get_option(AgsLv2OptionManager *option_manager,
  * Since: 0.7.128
  */
 void
-ags_lv2_option_manager_get_option(AgsLv2OptionManager *option_manager,
+ags_lv2_option_manager_get_option(AgsLv2OptionManager *lv2_option_manager,
 				  LV2_Handle instance,
 				  LV2_Options_Option* options,
 				  GError **error)
@@ -272,7 +282,7 @@ ags_lv2_option_manager_get_option(AgsLv2OptionManager *option_manager,
 }
 
 void
-ags_lv2_option_manager_real_set_option(AgsLv2OptionManager *option_manager,
+ags_lv2_option_manager_real_set_option(AgsLv2OptionManager *lv2_option_manager,
 				       LV2_Handle instance,
 				       LV2_Options_Option* options,
 				       GError **error)
@@ -282,7 +292,7 @@ ags_lv2_option_manager_real_set_option(AgsLv2OptionManager *option_manager,
 
 /**
  * ags_lv2_option_manager_set_option:
- * @option_manager: the #AgsLv2OptionManager
+ * @lv2_option_manager: the #AgsLv2OptionManager
  * @instance: the instance
  * @options: the options
  * @error: the #GError-struct
@@ -292,7 +302,7 @@ ags_lv2_option_manager_real_set_option(AgsLv2OptionManager *option_manager,
  * Since: 0.7.128
  */
 void
-ags_lv2_option_manager_set_option(AgsLv2OptionManager *option_manager,
+ags_lv2_option_manager_set_option(AgsLv2OptionManager *lv2_option_manager,
 				  LV2_Handle instance,
 				  LV2_Options_Option* options,
 				  GError **error)
