51c4f82bf38c687e182c183b78a94f7c53a63d9a
Joël Krähemann <jkraehemann@gmail.com>
2017-08-31 02:02:52 +0200

    add GNU Affero General Public License

--- ags/server/ags_server.h
+++ ags/server/ags_server.h
@@ -4,16 +4,16 @@
  * This file is part of GSequencer.
  *
  * GSequencer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
  * GSequencer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
  */
 
@@ -42,8 +42,7 @@
 #define AGS_TYPE_SERVER                (ags_server_get_type())
 #define AGS_SERVER(obj)                (G_TYPE_CHECK_INSTANCE_CAST((obj), AGS_TYPE_SERVER, AgsServer))
 #define AGS_SERVER_CLASS(class)        (G_TYPE_CHECK_CLASS_CAST(class, AGS_TYPE_SERVER, AgsServerClass))
-#define AGS_IS_SERVER(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_SERVER))
-#define AGS_IS_SERVER_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_SERVER))
+#define AGS_IS_SERVER(obj)             (G_TYPE_CHECK_INSTANCE_TYPE ((obj), AGS_TYPE_SERVER))#define AGS_IS_SERVER_CLASS(class)     (G_TYPE_CHECK_CLASS_TYPE ((class), AGS_TYPE_SERVER))
 #define AGS_SERVER_GET_CLASS(obj)      (G_TYPE_INSTANCE_GET_CLASS(obj, AGS_TYPE_SERVER, AgsServerClass))
 
 typedef struct _AgsServer AgsServer;
@@ -104,16 +103,6 @@ AgsServerInfo* ags_server_info_alloc(gchar *server_name);
 void ags_server_start(AgsServer *server);
 
 AgsServer* ags_server_lookup(AgsServerInfo *server_info);
-
-#ifdef AGS_WITH_XMLRPC_C
-xmlrpc_value* ags_server_create_object(xmlrpc_env *env,
-				       xmlrpc_value *param_array,
-				       void *server_info);
-
-xmlrpc_value* ags_server_object_set_property(xmlrpc_env *env,
-					     xmlrpc_value *param_array,
-					     void *server_info);
-#endif
 
 AgsServer* ags_server_new(GObject *application_context);
 
