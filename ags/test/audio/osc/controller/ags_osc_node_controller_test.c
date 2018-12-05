46abc9971eacd3b1745bc629e154b7721405db56
Joël Krähemann <jkraehemann@gmail.com>
2018-12-05 16:45:49 +0100

    fixed ags_osc_node_controller_test.c

--- ags/test/audio/osc/controller/ags_osc_node_controller_test.c
+++ ags/test/audio/osc/controller/ags_osc_node_controller_test.c
@@ -270,8 +270,6 @@ ags_osc_node_controller_test_get_data()
   guint padding;
   gboolean success;
 
-  GValue value = {0,};
-
   static const unsigned char *volume_message = "/node\x00\x00\x00,s\x00\x00/AgsSoundProvider/AgsAudio[\"test-drum\"]/AgsInput[0-1]/AgsVolumeChannel[0]/AgsPort[\"./volume[0]\"]:value\x00\x00\x00\x00";
 
   static const guint volume_message_size = 108;
@@ -292,10 +290,13 @@ ags_osc_node_controller_test_get_data()
   while(osc_response != NULL){
     gchar *address_pattern;
     gchar *type_tag;
+    gchar *path;
+
+    gfloat volume;
     
     ags_osc_buffer_util_get_message(AGS_OSC_RESPONSE(osc_response->data)->packet + 4,
 				    &address_pattern, &type_tag);
-
+    
     success = !g_strcmp0(address_pattern,
 			 "/node");
     
@@ -306,11 +307,31 @@ ags_osc_node_controller_test_get_data()
     success = !g_strcmp0(type_tag,
 			 ",sf");
     
-    if(!success){
-      g_message("%s", type_tag);
-      
+    if(!success){      
+      break; 
+   }
+
+    ags_osc_buffer_util_get_string(AGS_OSC_RESPONSE(osc_response->data)->packet + 16,
+				   &path, &length);
+
+    if(path == NULL){
+      success = FALSE;
+
       break;
     }
+
+    length = 4 * (guint) ceil((double) (length + 1) / 4.0);
+
+    ags_osc_buffer_util_get_float(AGS_OSC_RESPONSE(osc_response->data)->packet + 16 + length,
+				   &volume);
+
+    if(volume != 1.0){
+      success = FALSE;
+
+      break;      
+    }
+    
+    osc_response = osc_response->next;
   }
   
   CU_ASSERT(success);
