22ebcc73ae1a8186154ac6f8e98846b604d27c98
Joël Krähemann <jkraehemann@gmail.com>
2019-05-27 19:21:33 +0200

    completed API doc of ags_vst_audio_effect.h

--- ags/vst3-capi/public.sdk/source/vst/ags_vst_audio_effect.cpp
+++ ags/vst3-capi/public.sdk/source/vst/ags_vst_audio_effect.cpp
@@ -23,15 +23,6 @@
 
 extern "C" {
 
-  /**
-   * ags_vst_audio_effect_new:
-   * 
-   * Wrapper of AudioEffect constructor.
-   * 
-   * Returns: the #AgsVstAudioEffect-struct pointer.
-   * 
-   * Since: 2.2.0
-   */
   AgsVstAudioEffect* ags_vst_audio_effect_new()
   {
     return(new AudioEffect());
@@ -42,105 +33,106 @@ extern "C" {
     delete audio_effect;
   }
 
-  AgsVstAudioBus* ags_vst_audio_add_audio_input(AgsVstAudioEffect *audio_effect,
-						gchar *name, AgsVstSpeakerArrangement *arr, guint bus_type,
-						gint32 flags)
+  AgsVstAudioBus* ags_vst_audio_effect_add_audio_input(AgsVstAudioEffect *audio_effect,
+						       gchar *name, AgsVstSpeakerArrangement *arr, AgsVstBusType bus_type,
+						       gint32 flags)
   {
     return(audio_effect->addAudioInput(name, arr, static_cast<BusType>(bus_type),
 				       flags));
   }
   
-  AgsVstAudioBus* ags_vst_audio_add_audio_output(AgsVstAudioEffect *audio_effect,
-						 gchar *name, AgsVstSpeakerArrangement *arr, guint bus_type,
-						 gint32 flags)
+  AgsVstAudioBus* ags_vst_audio_effect_add_audio_output(AgsVstAudioEffect *audio_effect,
+							gchar *name, AgsVstSpeakerArrangement *arr, AgsVstBusType bus_type,
+							gint32 flags)
   {
     return(audio_effect->addAudioOutput(name, arr, static_cast<BusType>(bus_type),
 					flags));
   }
 
-  AgsVstAudioBus* ags_vst_audio_get_audio_input(AgsVstAudioEffect *audio_effect,
-						gint32 index)
+  AgsVstAudioBus* ags_vst_audio_effect_get_audio_input(AgsVstAudioEffect *audio_effect,
+						       gint32 index)
   {
     return(audio_effect->getAudioInput(index));
   }
   
-  AgsVstAudioBus* ags_vst_audio_get_audio_output(AgsVstAudioEffect *audio_effect,
-						 gint32 index)
+  AgsVstAudioBus* ags_vst_audio_effect_get_audio_output(AgsVstAudioEffect *audio_effect,
+							gint32 index)
   {
     return(audio_effect->getAudioOutput(index));
   }
 
-  AgsVstEventBus* ags_vst_audio_add_event_input(AgsVstAudioEffect *audio_effect,
-						gchar *name, gint32 channels, guint bus_type,
-						gint32 flags)
+  AgsVstEventBus* ags_vst_audio_effect_add_event_input(AgsVstAudioEffect *audio_effect,
+						       gchar *name, gint32 channels, AgsVstBusType bus_type,
+						       gint32 flags)
   {
     return(audio_effect->addEventInput(name, channels, static_cast<BusType>(bus_type),
 				       flags));
   }
 
-  AgsVstEventBus* ags_vst_audio_add_event_output(AgsVstAudioEffect *audio_effect,
-						 gchar *name, gint32 channels, guint bus_type,
-						 gint32 flags)
+  AgsVstEventBus* ags_vst_audio_effect_add_event_output(AgsVstAudioEffect *audio_effect,
+							gchar *name, gint32 channels, AgsVstBusType bus_type,
+							gint32 flags)
   {
     return(audio_effect->addEventOutput(name, channels, static_cast<BusType>(bus_type),
 					flags));
   }
 
-  AgsVstEventBus* ags_vst_audio_get_event_input(AgsVstAudioEffect *audio_effect,
-						gint32 index)
+  AgsVstEventBus* ags_vst_audio_effect_get_event_input(AgsVstAudioEffect *audio_effect,
+						       gint32 index)
   {
     return(audio_effect->getEventInput(index));
   }
 
-  AgsVstEventBus* ags_vst_audio_get_event_output(AgsVstAudioEffect *audio_effect,
-						 gint32 index)
+  AgsVstEventBus* ags_vst_audio_effect_get_event_output(AgsVstAudioEffect *audio_effect,
+							gint32 index)
   {
     return(audio_effect->getEventOutput(index));
   }
 
-  tresult PLUGIN_API ags_vst_audio_set_bus_arrangements(AgsVstAudioEffect *audio_effect,
-							AgsVstSpeakerArrangement *inputs, gint32 num_ins,
-							AgsVstSpeakerArrangement *outputs, gint32 num_outs)
+  gint32 ags_vst_audio_effect_set_bus_arrangements(AgsVstAudioEffect *audio_effect,
+						   AgsVstSpeakerArrangement *inputs, gint32 num_ins,
+						   AgsVstSpeakerArrangement *outputs, gint32 num_outs)
   {
   }
   
-  tresult PLUGIN_API ags_vst_audio_get_bus_arrangements(AgsVstAudioEffect *audio_effect,
-							guint bus_direction, gint32 bus_index,
-							AgsVstSpeakerArrangement **arr)
+  gint32 ags_vst_audio_effect_get_bus_arrangements(AgsVstAudioEffect *audio_effect,
+						   AgsVstBusDirection bus_direction, gint32 bus_index,
+						   AgsVstSpeakerArrangement **arr)
   {
     return(audio_effect->setBusArrangements(static_cast<BusDirection>(bus_direction), bus_index,
 					    static_cast<SpeakerArrangement&>(arr[0])));
   }
   
-  tresult PLUGIN_API ags_vst_audio_can_process_sample_size(AgsVstAudioEffect *audio_effect,
-							   gint32 symbolic_sample_size)
+  gint32 ags_vst_audio_effect_can_process_sample_size(AgsVstAudioEffect *audio_effect,
+						      gint32 symbolic_sample_size)
   {
     return(audio_effect->canProcessSampleSize(symbolic_sample_size));
   }
   
-  guint32 PLUGIN_API ags_vst_audio_get_latency_samples(AgsVstAudioEffect *audio_effect)
+  guint32 ags_vst_audio_effect_get_latency_samples(AgsVstAudioEffect *audio_effect)
   {
     return(audio_effect->getLatencySamples());
   }
   
-  tresult PLUGIN_API ags_vst_audio_setup_processing(AgsVstAudioEffect *audio_effect,
-						    AgsVstProcessSetup **setup)
+  gint32 ags_vst_audio_effect_setup_processing(AgsVstAudioEffect *audio_effect,
+					       AgsVstProcessSetup **setup)
   {
     return(audio_effect->setupProcessing(static_cast<ProcessSetup&>(setup[0])));
   }
   
-  tresult PLUGIN_API ags_vst_audio_set_processing(AgsVstAudioEffect *audio_effect,
-						  gboolean state)
+  gint32 ags_vst_audio_effect_set_processing(AgsVstAudioEffect *audio_effect,
+					     gboolean state)
   {
     return(audio_effect->setProcessing(static_cast<TBool>(state)));
   }
   
-  tresult PLUGIN_API ags_vst_audio_process(AgsVstProcessData **data)
+  gint32 ags_vst_audio_effect_process(AgsVstAudioEffect *audio_effect,
+				      AgsVstProcessData **data)
   {
     return(audio_effect->process(static_cast<ProcessData&>(data[0])));
   }
   
-  guint32 PLUGIN_API ags_vst_audio_get_tail_samples(AgsVstAudioEffect *audio_effect)
+  guint32 ags_vst_audio_effect_get_tail_samples(AgsVstAudioEffect *audio_effect)
   {
     return(audio_effect->getTailSamples());
   }
--- ags/vst3-capi/public.sdk/source/vst/ags_vst_audio_effect.h
+++ ags/vst3-capi/public.sdk/source/vst/ags_vst_audio_effect.h
@@ -32,48 +32,269 @@ extern "C" {
 
   typedef struct AgsVstAudioEffect AudioEffect;
   
+  /**
+   * ags_vst_audio_effect_new:
+   * 
+   * Wrapper of AudioEffect constructor.
+   * 
+   * Returns: the #AgsVstAudioEffect-struct pointer.
+   * 
+   * Since: 2.2.0
+   */
   AgsVstAudioEffect* ags_vst_audio_effect_new();
+
+  /**
+   * ags_vst_audio_effect_delete:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * 
+   * Delete @audio_effect.
+   * 
+   * Since: 2.2.0
+   */
   void ags_vst_audio_effect_delete(AgsVstAudioEffect *audio_effect);
 
-  AgsVstAudioBus* ags_vst_audio_add_audio_input(AgsVstAudioEffect *audio_effect,
-						gchar *name, AgsVstSpeakerArrangement *arr, guint bus_type,
-						gint32 flags);
-  AgsVstAudioBus* ags_vst_audio_add_audio_output(AgsVstAudioEffect *audio_effect,
-						 gchar *name, AgsVstSpeakerArrangement *arr, guint bus_type,
-						 gint32 flags);
+  /**
+   * ags_vst_audio_effect_add_audio_input:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @name: the name as string
+   * @arr: the #AgsVstSpeakerArrangement-struct pointer
+   * @bus_type: the bus type as #AgsVstBusType-enum
+   * @flags: the flags
+   * 
+   * Add audio input with @name and @arr of @bus_type using @flags.
+   * 
+   * Returns: the #AgsVstAudioBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstAudioBus* ags_vst_audio_effect_add_audio_input(AgsVstAudioEffect *audio_effect,
+						       gchar *name, AgsVstSpeakerArrangement *arr, AgsVstBusType bus_type,
+						       gint32 flags);
 
-  AgsVstAudioBus* ags_vst_audio_get_audio_input(AgsVstAudioEffect *audio_effect,
-						gint32 index);
-  AgsVstAudioBus* ags_vst_audio_get_audio_output(AgsVstAudioEffect *audio_effect,
-						 gint32 index);
+  /**
+   * ags_vst_audio_effect_add_audio_output:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @name: the name as string
+   * @arr: the #AgsVstSpeakerArrangement-struct pointer
+   * @bus_type: the bus type as #AgsVstBusType-enum
+   * @flags: the flags
+   * 
+   * Add audio output with @name and @arr of @bus_type using @flags.
+   * 
+   * Returns: the #AgsVstAudioBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstAudioBus* ags_vst_audio_effect_add_audio_output(AgsVstAudioEffect *audio_effect,
+							gchar *name, AgsVstSpeakerArrangement *arr, AgsVstBusType bus_type,
+							gint32 flags);
 
-  AgsVstEventBus* ags_vst_audio_add_event_input(AgsVstAudioEffect *audio_effect,
-						gchar *name, gint32 channels, guint bus_type,
-						gint32 flags);
-  AgsVstEventBus* ags_vst_audio_add_event_output(AgsVstAudioEffect *audio_effect,
-						 gchar *name, gint32 channels, guint bus_type,
-						 gint32 flags);
+  /**
+   * ags_vst_audio_effect_get_audio_input:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @index: the index to get
+   * 
+   * Get audio input by @index.
+   * 
+   * Returns: the #AgsVstAudioBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstAudioBus* ags_vst_audio_effect_get_audio_input(AgsVstAudioEffect *audio_effect,
+						       gint32 index);
 
-  AgsVstEventBus* ags_vst_audio_get_event_input(AgsVstAudioEffect *audio_effect,
-						gint32 index);
-  AgsVstEventBus* ags_vst_audio_get_event_output(AgsVstAudioEffect *audio_effect,
-						 gint32 index);
+  /**
+   * ags_vst_audio_effect_get_audio_output:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @index: the index to get
+   * 
+   * Get audio output by @index.
+   * 
+   * Returns: the #AgsVstAudioBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstAudioBus* ags_vst_audio_effect_get_audio_output(AgsVstAudioEffect *audio_effect,
+							gint32 index);
 
-  tresult PLUGIN_API ags_vst_audio_set_bus_arrangements(AgsVstAudioEffect *audio_effect,
-							AgsVstSpeakerArrangement *inputs, gint32 num_ins,
-							AgsVstSpeakerArrangement *outputs, gint32 num_outs);
-  tresult PLUGIN_API ags_vst_audio_get_bus_arrangements(AgsVstAudioEffect *audio_effect,
-							guint bus_direction, gint32 bus_index,
-							AgsVstSpeakerArrangement **arr);
-  tresult PLUGIN_API ags_vst_audio_can_process_sample_size(AgsVstAudioEffect *audio_effect,
-							   gint32 symbolic_sample_size);
-  guint32 PLUGIN_API ags_vst_audio_get_latency_samples(AgsVstAudioEffect *audio_effect);
-  tresult PLUGIN_API ags_vst_audio_setup_processing(AgsVstAudioEffect *audio_effect,
-						    AgsVstProcessSetup **setup);
-  tresult PLUGIN_API ags_vst_audio_set_processing(AgsVstAudioEffect *audio_effect,
-						  gboolean state);
-  tresult PLUGIN_API ags_vst_audio_process(AgsVstProcessData **data);
-  guint32 PLUGIN_API ags_vst_audio_get_tail_samples(AgsVstAudioEffect *audio_effect);
+  /**
+   * ags_vst_audio_effect_add_event_input:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @name: the name as string
+   * @channels: channels
+   * @bus_type: the bus type as #AgsVstBusType-enum
+   * @flags: the flags
+   * 
+   * Add event input with @name and @channesl of @bus_type using @flags.
+   * 
+   * Returns: the #AgsVstEventBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstEventBus* ags_vst_audio_effect_add_event_input(AgsVstAudioEffect *audio_effect,
+						       gchar *name, gint32 channels, AgsVstBusType bus_type,
+						       gint32 flags);
+
+  /**
+   * ags_vst_audio_effect_add_event_output:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @name: the name as string
+   * @channels: channels
+   * @bus_type: the bus type as #AgsVstBusType-enum
+   * @flags: the flags
+   * 
+   * Add event output with @name and @channesl of @bus_type using @flags.
+   * 
+   * Returns: the #AgsVstEventBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstEventBus* ags_vst_audio_effect_add_event_output(AgsVstAudioEffect *audio_effect,
+							gchar *name, gint32 channels, AgsVstBusType bus_type,
+							gint32 flags);
+
+  /**
+   * ags_vst_audio_effect_get_event_input:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @index: the index to get
+   * 
+   * Get event input by @index.
+   * 
+   * Returns: the #AgsVstEventBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstEventBus* ags_vst_audio_effect_get_event_input(AgsVstAudioEffect *audio_effect,
+						       gint32 index);
+
+  /**
+   * ags_vst_audio_effect_get_event_output:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @index: the index to get
+   * 
+   * Get event output by @index.
+   * 
+   * Returns: the #AgsVstEventBus-struct pointer
+   * 
+   * Since: 2.2.0
+   */
+  AgsVstEventBus* ags_vst_audio_effect_get_event_output(AgsVstAudioEffect *audio_effect,
+							gint32 index);
+  
+  /**
+   * ags_vst_audio_effect_set_bus_arrangements:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @inputs: the #AgsVstSpeakerArrangement-struct pointer
+   * @num_ins: number of inputs as 32 bit integer
+   * @outputs: the #AgsVstSpeakerArrangement-struct pointer
+   * @num_outs: number of outputs as 32 bit integer
+   * 
+   * Set bus arrangements of @audio_effect.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_set_bus_arrangements(AgsVstAudioEffect *audio_effect,
+						   AgsVstSpeakerArrangement *inputs, gint32 num_ins,
+						   AgsVstSpeakerArrangement *outputs, gint32 num_outs);
+
+  /**
+   * ags_vst_audio_effect_get_bus_arrangements:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @bus_direction: the #AgsVstBusDirection-enum
+   * @bus_index: the bus index
+   * @arr: the #AgsVstSpeakerArrangement-struct pointer to a pointer
+   * 
+   * Get bus arrangements of @audio_effect.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_get_bus_arrangements(AgsVstAudioEffect *audio_effect,
+						   AgsVstBusDirection bus_direction, gint32 bus_index,
+						   AgsVstSpeakerArrangement **arr);
+
+  /**
+   * ags_vst_audio_effect_can_process_sample_size:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @symbolic_sample_size: symbolic sample size
+   * 
+   * Can @audio_effect process @symbolic_sample_size.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_can_process_sample_size(AgsVstAudioEffect *audio_effect,
+						      gint32 symbolic_sample_size);
+
+  /**
+   * ags_vst_audio_effect_get_latency_samples:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * 
+   * Get lantency samples of @audio_effect.
+   * 
+   * Returns: the latency
+   * 
+   * Since: 2.2.0
+   */
+  guint32 ags_vst_audio_effect_get_latency_samples(AgsVstAudioEffect *audio_effect);
+
+  /**
+   * ags_vst_audio_effect_setup_processing:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @setup: the #AgsVstProcessSetup-struct pointer to a pointer
+   * 
+   * Setup processing of @audio_effect.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_setup_processing(AgsVstAudioEffect *audio_effect,
+					       AgsVstProcessSetup **setup);
+
+  /**
+   * ags_vst_audio_effect_set_processing:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @state: %TRUE if processing, otherwise %FALSE
+   * 
+   * Set processing of @audio_effect.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_set_processing(AgsVstAudioEffect *audio_effect,
+					     gboolean state);
+
+  /**
+   * ags_vst_audio_effect_process:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * @data: the #AgsVstProcessData-struct pointer to a pointer
+   * 
+   * Process @audio_effect using @data.
+   * 
+   * Returns: the result code
+   * 
+   * Since: 2.2.0
+   */
+  gint32 ags_vst_audio_effect_process(AgsVstAudioEffect *audio_effect,
+				      AgsVstProcessData **data);
+
+  /**
+   * ags_vst_audio_effect_get_tail_samples:
+   * @audio_effect: the #AgsVstAudioEffect-struct pointer
+   * 
+   * Get tail samples of @audio_effect.
+   * 
+   * Returns: the tail samples
+   * 
+   * Since: 2.2.0
+   */
+  guint32 ags_vst_audio_effect_get_tail_samples(AgsVstAudioEffect *audio_effect);
   
 #ifdef __cplusplus
 }
