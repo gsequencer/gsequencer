c3124cabc715660cb13d66b93e008920c7c72fcf
Joël Krähemann <jkraehemann@gmail.com>
2019-05-28 17:36:41 +0200

    completed API doc of ags_vst_component.h

--- ags/vst3-capi/public.sdk/source/vst/ags_vst_component.cpp
+++ ags/vst3-capi/public.sdk/source/vst/ags_vst_component.cpp
@@ -36,104 +36,93 @@ extern "C" {
   }
 
   void ags_vst_component_set_controller_class(AgsVstComponent *component,
-					      FUID **cid)
+					      AgsVstFUID **cid)
   {
     component->setControllerClass(static_cast<FUID&>(cid[0]));
   }
 
-  tresult ags_vst_component_remove_audio_buses(AgsVstComponent *component) 
+  gint32 ags_vst_component_remove_audio_buses(AgsVstComponent *component) 
   {
     return(component->removeAudioBuses());
   }
 
-  tresult ags_vst_component_remove_event_buses(AgsVstComponent *component) 
+  gint32 ags_vst_component_remove_event_buses(AgsVstComponent *component) 
   {
     return(component->removeEventBuses());
   }
 
-  tresult ags_vst_component_rename_bus(AgsVstComponent *component,
-				       guint type, guint dir, gint32 index,
-				       AgsVstString128 new_name)
+  gint32 ags_vst_component_rename_bus(AgsVstComponent *component,
+				      AgsVstMediaType type, AgsVstBusDirection dir, gint32 index,
+				      AgsVstString128 new_name)
   {
-    gint length;
-
-    length = strlen(new_name);
-
-    //FIXME:JK: memory-leak
-    if(length > 127){
-      new_name = g_strndup(new_name,
-			   127);
-    }
-        
-    //FIXME:JK: type mismatch of parameter new_name
     return(component->renameBus(static_cast<MediaType>(type), static_cast<BusDirection>(dir), index,
 				new_name));
   }
 
-  tresult PLUGIN_API ags_vst_component_get_controller_class_id(AgsVstComponent *component,
-							       void *class_id)
+  gint32 ags_vst_component_get_controller_class_id(AgsVstComponent *component,
+						   AgsVstTUID class_id)
   {
     return(component->getControllerClassId(static_cast<TUID>(class_id)));
   }
 
-  tresult PLUGIN_API ags_vst_component_set_io_mode(AgsVstComponent *component,
-						   guint mode)
+  gint32 ags_vst_component_set_io_mode(AgsVstComponent *component,
+				       AgsVstIoMode mode)
   {
     return(component->setIoMode(static_cast<IoMode>(mode)));
   }
 
-  gint32 PLUGIN_API ags_vst_component_get_bus_count(AgsVstComponent *component,
-						    guint type, guint dir)
+  gint32 ags_vst_component_get_bus_count(AgsVstComponent *component,
+					 AgsVstMediaType type, AgsVstBusDirection dir)
   {
     return(component->getBusCount(static_cast<MediaType>(type), static_cast<BusDirection>(dir)));
   }
 
-  tresult PLUGIN_API ags_vst_component_get_bus_info(AgsVstComponent *component,
-						    guint type, guint dir, gint32 index, AgsVstBusInfo **info)
+  gint32 ags_vst_component_get_bus_info(AgsVstComponent *component,
+					AgsVstMediaType type, AgsVstBusDirection dir, gint32 index, AgsVstBusInfo **info)
   {
     return(component->getBusInfo(static_cast<MediaType>(type), static_cast<BusDirection>(dir), index,
 				 static_cast<BusInfo&>(info[0])));
   }
 
-  tresult PLUGIN_API ags_vst_component_get_routing_info(AgsVstComponent *component,
-							AgsVstRoutingInfo **in_info, AgsVstRoutingInfo **out_info)
+  gint32 ags_vst_component_get_routing_info(AgsVstComponent *component,
+					    AgsVstRoutingInfo **in_info, AgsVstRoutingInfo **out_info)
   {
     return(component->getRoutingInfo(static_cast<RoutingInfo&>(in_info[0]), static_cast<RoutingInfo&>(out_info[0])));
   }
 
-  tresult PLUGIN_API ags_vst_component_activate_bus(AgsVstComponent *component,
-						    guint type, guint dir, gint32 index, gboolean state)
+  gint32 ags_vst_component_activate_bus(AgsVstComponent *component,
+					AgsVstMediaType type, AgsVstBusDirection dir, gint32 index, gboolean state)
   {
     return(component->activateBus(static_cast<MediaType>(type), static_cast<BusDirection>(dir), index,
 				  static_cast<TBool>(state)));
   }
 
-  tresult PLUGIN_API ags_vst_component_set_active(AgsVstComponent *component,
-						  gboolean state)
+  gint32 ags_vst_component_set_active(AgsVstComponent *component,
+				      gboolean state)
   {
     return(component->setActive(static_cast<TBool>(state)));
   }
 
-  tresult PLUGIN_API ags_vst_component_set_state(AgsVstComponent *component,
-						 AgsVstIBStream *state)
+  gint32 ags_vst_component_set_state(AgsVstComponent *component,
+				     AgsVstIBStream *state)
   {
     return(component->setState(state));
   }
 
-  tresult PLUGIN_API ags_vst_component_get_state(AgsVstComponent *component,
-						 AgsVstIBStream *state)
+  gint32 ags_vst_component_get_state(AgsVstComponent *component,
+				     AgsVstIBStream *state)
 
   {
     return(component->getState(state));
   }
 
-  tresult PLUGIN_API ags_vst_component_initialize(AgsVstComponent *component,
-						  AgsVstFUnknown *context)
+  gint32 ags_vst_component_initialize(AgsVstComponent *component,
+				      AgsVstFUnknown *context)
   {
     return(component->initialize(context));
   }
   
-  tresult PLUGIN_API ags_vst_component_terminate(AgsVstComponent *component)
+  gint32 ags_vst_component_terminate(AgsVstComponent *component)
   {
     return(component->terminate());
   }
