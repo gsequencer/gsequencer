f1a6dbaa2b31b3fb737fedf2c0abb8ba84cca5ab
Joël Krähemann <jkraehemann@gmail.com>
2019-05-30 00:14:44 +0200

    added ags_vst_ierror_context.h

---
+++ ags/vst3-capi/plugininterfaces/base/ags_vst_ierror_context.h
@@ -0,0 +1,39 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2019 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __AGS_VST_IERROR_CONTEXT_H__
+#define __AGS_VST_IERROR_CONTEXT_H__
+
+#include <ags/vst3-capi/pluginterfaces/base/funknown.h>
+
+#define AGS_VST_IERROR_CONTEXT_IID ((AgsVstTUID) IError_Context__iid);
+
+#ifdef __cplusplus
+extern "C" {
+
+  typedef struct AgsVstIErrorContext IErrorContext;
+
+  gint32 ags_vst_ierror_context_get_error_message(AgsVstIErrorContext *error_context);
+  
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__AGS_VST_IERROR_CONTEXT_H__*/
