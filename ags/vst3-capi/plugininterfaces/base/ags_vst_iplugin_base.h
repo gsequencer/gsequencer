d41fdf61798e9f875a0b6538c475caf9189acbdc
Joël Krähemann <jkraehemann@gmail.com>
2019-05-30 05:45:38 +0200

    implemented AgsVstIPluginBase

---
+++ ags/vst3-capi/plugininterfaces/base/ags_vst_iplugin_base.h
@@ -0,0 +1,45 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2019 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __AGS_VST_IPLUGIN_BASE_H__
+#define __AGS_VST_IPLUGIN_BASE_H__
+
+#include <ags/vst3-capi/pluginterfaces/base/funknown.h>
+#include <ags/vst3-capi/pluginterfaces/base/fstrdefs.h>
+
+#define AGS_VST_IPLUGIN_BASE_IID (ags_vst_iplugin_base_get_iid());
+
+#ifdef __cplusplus
+extern "C" {
+
+  typedef struct AgsVstIPluginBase IPluginBase;
+
+  AgsVstTUID ags_vst_iplugin_base_get_iid();
+
+  gint32 ags_vst_iplugin_base_initialize(AgsVstIPluginBase *plugin_base,
+					 AgsVstFUnknown *context);
+
+  gint32 ags_vst_iplugin_base_terminate(AgsVstIPluginBase *plugin_base);
+
+#endif
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*__AGS_VST_IPLUGIN_BASE_H__*/
