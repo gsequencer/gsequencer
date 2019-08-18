6c5131abbd2d127ccea31a89110762be17647698
Joël Krähemann <jkraehemann@gmail.com>
2019-08-18 15:11:38 +0200

    added skelleton of ags_filter_util.h and ags_fourier_transform_util.h

---
+++ ags/audio/ags_fourier_transform_util.c
@@ -0,0 +1,94 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2019 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ags/audio/ags_fourier_transform_util.h>
+
+#include <math.h>
+
+void
+ags_fourier_transform_util_compute_s8(gint8 *buffer,
+				      guint buffer_length,
+				      guint samplerate,
+				      gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_s16(gint16 *buffer,
+				       guint buffer_length,
+				       guint samplerate,
+				       gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_s24(gint24 *buffer,
+				       guint buffer_length,
+				       guint samplerate,
+				       gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_s32(gint32 *buffer,
+				       guint buffer_length,
+				       guint samplerate,
+				       gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_s64(gint64 *buffer,
+				       guint buffer_length,
+				       guint samplerate,
+				       gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_float(gfloat *buffer,
+					 guint buffer_length,
+					 guint samplerate,
+					 gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_double(gdouble *buffer,
+					  guint buffer_length,
+					  guint samplerate,
+					  gdouble *result)
+{
+  //TODO:JK: implement me
+}
+
+void
+ags_fourier_transform_util_compute_complex(AgsComplex *buffer,
+					   guint buffer_length,
+					   guint samplerate,
+					   gdouble *result)
+{
+  //TODO:JK: implement me
+}
---
+++ ags/audio/ags_fourier_transform_util.h
@@ -0,0 +1,59 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2019 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef __AGS_FOURIER_TRANSFORM_UTIL_H__
+#define __AGS_FOURIER_TRANSFORM_UTIL_H__
+
+#include <glib.h>
+#include <glib-object.h>
+
+void ags_fourier_transform_util_compute_s8(gint8 *buffer,
+					   guint buffer_length,
+					   guint samplerate,
+					   gdouble *result);
+void ags_fourier_transform_util_compute_s16(gint16 *buffer,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble *result);
+void ags_fourier_transform_util_compute_s24(gint24 *buffer,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble *result);
+void ags_fourier_transform_util_compute_s32(gint32 *buffer,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble *result);
+void ags_fourier_transform_util_compute_s64(gint64 *buffer,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble *result);
+void ags_fourier_transform_util_compute_float(gfloat *buffer,
+					      guint buffer_length,
+					      guint samplerate,
+					      gdouble *result);
+void ags_fourier_transform_util_compute_double(gdouble *buffer,
+					       guint buffer_length,
+					       guint samplerate,
+					       gdouble *result);
+void ags_fourier_transform_util_compute_complex(AgsComplex *buffer,
+						guint buffer_length,
+						guint samplerate,
+						gdouble *result);
+
+#endif /*__AGS_FOURIER_TRANSFORM_UTIL_H__*/
