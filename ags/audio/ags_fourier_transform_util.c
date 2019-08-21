2f79e05bccf6846e135bdaa631f09f7b1fbf377d
Joël Krähemann <jkraehemann@gmail.com>
2019-08-21 19:22:00 +0200

    implementing ags_fourier_transform_util.c

--- ags/audio/ags_fourier_transform_util.c
+++ ags/audio/ags_fourier_transform_util.c
@@ -24,7 +24,7 @@
 #include <math.h>
 
 /**
- * ags_fourier_transform_util_compute_s8:
+ * ags_fourier_transform_util_compute_stft_s8:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -36,10 +36,10 @@
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_s8(gint8 *buffer, guint channels,
-				      guint buffer_length,
-				      guint samplerate,
-				      gdouble **retval)
+ags_fourier_transform_util_compute_stft_s8(gint8 *buffer, guint channels,
+					   guint buffer_length,
+					   guint samplerate,
+					   gdouble **retval)
 {
   complex m_e;
   guint i;
@@ -75,7 +75,7 @@ ags_fourier_transform_util_compute_s8(gint8 *buffer, guint channels,
 }
 
 /**
- * ags_fourier_transform_util_compute_s16:
+ * ags_fourier_transform_util_compute_stft_s16:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -87,16 +87,16 @@ ags_fourier_transform_util_compute_s8(gint8 *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_s16(gint16 *buffer, guint channels,
-				       guint buffer_length,
-				       guint samplerate,
-				       gdouble **retval)
+ags_fourier_transform_util_compute_stft_s16(gint16 *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_s24:
+ * ags_fourier_transform_util_compute_stft_s24:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -108,16 +108,16 @@ ags_fourier_transform_util_compute_s16(gint16 *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_s24(gint32 *buffer, guint channels,
-				       guint buffer_length,
-				       guint samplerate,
-				       gdouble **retval)
+ags_fourier_transform_util_compute_stft_s24(gint32 *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_s32:
+ * ags_fourier_transform_util_compute_stft_s32:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -129,16 +129,16 @@ ags_fourier_transform_util_compute_s24(gint32 *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_s32(gint32 *buffer, guint channels,
-				       guint buffer_length,
-				       guint samplerate,
-				       gdouble **retval)
+ags_fourier_transform_util_compute_stft_s32(gint32 *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_s64:
+ * ags_fourier_transform_util_compute_stft_s64:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -150,16 +150,16 @@ ags_fourier_transform_util_compute_s32(gint32 *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_s64(gint64 *buffer, guint channels,
-				       guint buffer_length,
-				       guint samplerate,
-				       gdouble **retval)
+ags_fourier_transform_util_compute_stft_s64(gint64 *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_float:
+ * ags_fourier_transform_util_compute_stft_float:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -171,16 +171,16 @@ ags_fourier_transform_util_compute_s64(gint64 *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_float(gfloat *buffer, guint channels,
-					 guint buffer_length,
-					 guint samplerate,
-					 gdouble **retval)
+ags_fourier_transform_util_compute_stft_float(gfloat *buffer, guint channels,
+					      guint buffer_length,
+					      guint samplerate,
+					      gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_double:
+ * ags_fourier_transform_util_compute_stft_double:
  * @buffer: the audio buffer
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
@@ -192,31 +192,184 @@ ags_fourier_transform_util_compute_float(gfloat *buffer, guint channels,
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_double(gdouble *buffer, guint channels,
-					  guint buffer_length,
-					  guint samplerate,
-					  gdouble **retval)
+ags_fourier_transform_util_compute_stft_double(gdouble *buffer, guint channels,
+					       guint buffer_length,
+					       guint samplerate,
+					       gdouble **retval)
 {
   //TODO:JK: implement me
 }
 
 /**
- * ags_fourier_transform_util_compute_complex:
- * @buffer: the audio buffer
+ * ags_fourier_transform_util_inverse_stft_s8:
+ * @buffer: the complex data
  * @channels: number of audio channels
  * @buffer_length: the buffer's length
  * @samplerate: the samplerate
  * @retval: the return location of result
  * 
- * Compute fourier transform for @buffer.
+ * Compute inverse fourier transform for @buffer.
  * 
  * Since: 2.3.0
  */
 void
-ags_fourier_transform_util_compute_complex(AgsComplex *buffer, guint channels,
+ags_fourier_transform_util_inverse_stft_s8(AgsComplex *buffer, guint channels,
 					   guint buffer_length,
 					   guint samplerate,
-					   gdouble **retval)
+					   gint8 **retval)
+{
+  complex m_e;
+  guint i;
+
+  static const gdouble scale = 127.0;
+
+
+  if(buffer == NULL ||
+     retval == NULL){
+    return;
+  }
+
+  if(retval[0] == NULL){
+    retval[0] = (gint8 *) malloc(buffer_length * sizeof(gint8));
+  }
+  
+  m_e = M_E + I * 0.0;
+
+  for(i = 0; i < buffer_length; i++){
+    complex z;
+    gdouble k;
+    gdouble y;
+
+    z = ags_complex_get(buffer[i]);
+
+    k = (gdouble) i;
+
+    y = (1.0 / buffer_length) * (z * cpow(m_e, I * 2.0 * M_PI * k * i / buffer_length);
+  }
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_s16:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_s16(AgsComplex *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gint16 **retval)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_s24:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_s24(AgsComplex *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gint32 **retval)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_s32:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_s32(AgsComplex *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gint32 **retval)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_s64:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_s64(AgsComplex *buffer, guint channels,
+					    guint buffer_length,
+					    guint samplerate,
+					    gint64 **retval)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_float:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_float(AgsComplex *buffer, guint channels,
+					      guint buffer_length,
+					      guint samplerate,
+					      gfloat **retval)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fourier_transform_util_inverse_stft_double:
+ * @buffer: the complex data
+ * @channels: number of audio channels
+ * @buffer_length: the buffer's length
+ * @samplerate: the samplerate
+ * @retval: the return location of result
+ * 
+ * Compute inverse fourier transform for @buffer.
+ * 
+ * Since: 2.3.0
+ */
+void
+ags_fourier_transform_util_inverse_stft_double(AgsComplex *buffer, guint channels,
+					       guint buffer_length,
+					       guint samplerate,
+					       gdouble **retval)
 {
   //TODO:JK: implement me
 }
