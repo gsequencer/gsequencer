ea04635af588689c7c30d89f4350eb092e762d74
Joël Krähemann <jkraehemann@gmail.com>
2020-04-30 16:45:33 +0200

    added tests to ags_fx_analyse_audio_signal_test.c

--- ags/test/audio/fx/ags_fx_analyse_audio_signal_test.c
+++ ags/test/audio/fx/ags_fx_analyse_audio_signal_test.c
@@ -33,6 +33,7 @@ int ags_fx_analyse_audio_signal_test_init_suite();
 int ags_fx_analyse_audio_signal_test_clean_suite();
 
 void ags_fx_analyse_audio_signal_test_new();
+void ags_fx_analyse_audio_signal_test_run_inter();
 
 /* The suite initialization function.
  * Opens the temporary file used by the tests.
@@ -69,6 +70,107 @@ ags_fx_analyse_audio_signal_test_new()
   CU_ASSERT(AGS_RECALL_AUDIO_SIGNAL(fx_analyse_audio_signal)->source == audio_signal);
 }
 
+void
+ags_fx_analyse_audio_signal_test_run_inter()
+{
+  AgsAudio *audio;
+  AgsChannel *channel;
+  AgsRecycling *recycling;
+  AgsAudioSignal *audio_signal;
+  AgsRecallContainer *recall_container;
+  AgsFxAnalyseAudio *fx_analyse_audio;
+  AgsFxAnalyseAudioProcessor *fx_analyse_audio_processor;
+  AgsFxAnalyseChannel *fx_analyse_channel;
+  AgsFxAnalyseChannelProcessor *fx_analyse_channel_processor;
+  AgsFxAnalyseRecycling *fx_analyse_recycling;
+  AgsFxAnalyseAudioSignal *fx_analyse_audio_signal;
+
+  
+  /* audio */
+  audio = g_object_new(AGS_TYPE_AUDIO,
+		       NULL);
+
+  recall_container = ags_recall_container_new();
+  ags_audio_add_recall_container(audio,
+				 recall_container);
+  
+  fx_analyse_audio = ags_fx_analyse_audio_new(audio);
+  ags_recall_container_add(recall_container,
+			   fx_analyse_audio);
+  
+  CU_ASSERT(fx_analyse_audio != NULL);
+
+  /* audio processor */  
+  fx_analyse_audio_processor = ags_fx_analyse_audio_processor_new(audio);
+
+  g_object_set(fx_analyse_audio_processor,
+	       "recall-audio", fx_analyse_audio,
+	       NULL);
+  
+  ags_recall_container_add(recall_container,
+			   fx_analyse_audio_processor);
+
+  CU_ASSERT(fx_analyse_audio_processor != NULL);
+
+  /* channel */
+  channel = g_object_new(AGS_TYPE_CHANNEL,
+			 NULL);
+
+  ags_channel_add_recall_container(channel,
+				   recall_container);
+  
+  fx_analyse_channel = ags_fx_analyse_channel_new(channel);
+
+  g_object_set(fx_analyse_channel,
+	       "recall-audio", fx_analyse_audio,
+	       NULL);
+
+  ags_recall_container_add(recall_container,
+			   fx_analyse_channel);
+
+  CU_ASSERT(fx_analyse_channel != NULL);
+
+  /* channel processor */  
+  fx_analyse_channel_processor = ags_fx_analyse_channel_processor_new(channel);
+
+  g_object_set(fx_analyse_channel_processor,
+	       "recall-audio", fx_analyse_audio,
+	       "recall-channel", fx_analyse_channel,
+	       NULL);
+
+  ags_recall_container_add(recall_container,
+			   fx_analyse_channel_processor);
+
+  CU_ASSERT(fx_analyse_channel_processor != NULL);
+
+  /* recycling */
+  recycling = g_object_new(AGS_TYPE_RECYCLING,
+			   NULL);
+  
+  fx_analyse_recycling = ags_fx_analyse_recycling_new(recycling);
+
+  ags_recall_add_child(fx_analyse_channel_processor,
+		       fx_analyse_recycling);
+  
+  CU_ASSERT(fx_analyse_recycling != NULL);
+
+  /* audio signal */
+  audio_signal = g_object_new(AGS_TYPE_AUDIO_SIGNAL,
+			      NULL);
+  
+  fx_analyse_audio_signal = ags_fx_analyse_audio_signal_new(audio_signal);
+
+  ags_recall_add_child(fx_analyse_recycling,
+		       fx_analyse_audio_signal);
+
+  CU_ASSERT(fx_analyse_audio_signal != NULL);
+
+  /* run inter */
+  //TODO:JK: improve me
+  
+  ags_recall_run_inter(fx_analyse_audio_signal);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -96,7 +198,8 @@ main(int argc, char **argv)
   }
 
   /* add the tests to the suite */
-  if((CU_add_test(pSuite, "test of AgsFxAnalyseAudioSignal new", ags_fx_analyse_audio_signal_test_new) == NULL)){
+  if((CU_add_test(pSuite, "test of AgsFxAnalyseAudioSignal new", ags_fx_analyse_audio_signal_test_new) == NULL) ||
+     (CU_add_test(pSuite, "test of AgsFxAnalyseAudioSignal run inter", ags_fx_analyse_audio_signal_test_run_inter) == NULL)){
     CU_cleanup_registry();
     
     return CU_get_error();
