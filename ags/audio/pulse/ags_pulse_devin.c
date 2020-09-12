96651ed8ba543453b180412f293b53e033e38480
Joël Krähemann <jkraehemann@gmail.com>
2020-09-13 00:45:51 +0200

    fixed tempo issue

--- ags/audio/pulse/ags_pulse_devin.c
+++ ags/audio/pulse/ags_pulse_devin.c
@@ -2093,7 +2093,7 @@ ags_pulse_devin_tic(AgsSoundcard *soundcard)
 
   g_rec_mutex_unlock(pulse_devin_mutex);
 
-  if(delay_counter + 1.0 >= delay){
+  if(delay_counter + 1.0 >= floor(delay)){
     if(do_loop &&
        note_offset + 1 == loop_right){
       ags_soundcard_set_note_offset(soundcard,
