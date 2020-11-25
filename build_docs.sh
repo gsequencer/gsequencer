96342418e9a47661bb0f814284043e7953ea7923
Joël Krähemann <jkraehemann@gmail.com>
2020-11-25 20:25:32 +0100

    improved meson build manual

--- build_docs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-xsltproc --nonet --xinclude -o ${MESON_BUILD_ROOT}/html/user-docs/ http://docbook.sourceforge.net/release/xsl/current/htmlhelp/htmlhelp.xsl docs/usersBook.xml
-xsltproc --nonet --xinclude -o ${MESON_BUILD_ROOT}/html/developer-docs/ http://docbook.sourceforge.net/release/xsl/current/htmlhelp/htmlhelp.xsl docs/developersBook.xml
-xsltproc --nonet --xinclude -o ${MESON_BUILD_ROOT}/html/osc-docs/ http://docbook.sourceforge.net/release/xsl/current/htmlhelp/htmlhelp.xsl docs/oscBook.xml
