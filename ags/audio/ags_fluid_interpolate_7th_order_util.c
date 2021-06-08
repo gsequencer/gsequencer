95205fb61c519a88c599f13d2fab97fc5fddba42
Joël Krähemann <jkraehemann@gmail.com>
2021-06-08 10:22:05 +0200

    added fluidsynth utility functions skelletons

---
+++ ags/audio/ags_fluid_interpolate_7th_order_util.c
@@ -0,0 +1,191 @@
+/* GSequencer - Advanced GTK Sequencer
+ * Copyright (C) 2005-2021 Joël Krähemann
+ *
+ * This file is part of GSequencer.
+ *
+ * GSequencer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GSequencer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GSequencer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <ags/audio/ags_fluid_interpolate_7th_order_util.h>
+
+/**
+ * SECTION:ags_fluid_interpolate_7th_order_util
+ * @short_description: util functions to fluid interpolate 7th_order
+ * @title: AgsFluidInterpolate7th_OrderUtil
+ * @section_id:
+ * @include: ags/audio/ags_fluid_interpolate_7th_order_util.h
+ *
+ * These utility functions allow you to fill fluid interpolated
+ * 7th order data.
+ */
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_s8:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_s8(gint8 *destination,
+					     gint8 *source,
+					     guint buffer_length,
+					     gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_s16:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_s16(gint16 *destination,
+					      gint16 *source,
+					      guint buffer_length,
+					      gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_s24:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_s24(gint32 *destination,
+					      gint32 *source,
+					      guint buffer_length,
+					      gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_s32:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_s32(gint32 *destination,
+					      gint32 *source,
+					      guint buffer_length,
+					      gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_s64:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_s64(gint64 *destination,
+					      gint64 *source,
+					      guint buffer_length,
+					      gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_float:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_float(gfloat *destination,
+						gfloat *source,
+						guint buffer_length,
+						gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_double:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_double(gdouble *destination,
+						 gdouble *source,
+						 guint buffer_length,
+						 gdouble factor)
+{
+  //TODO:JK: implement me
+}
+
+/**
+ * ags_fluid_interpolate_7th_order_util_fill_complex:
+ * @destination: the destination audio buffer
+ * @source: the source audio buffer
+ * @buffer_length: the buffer length
+ * @factor: the factor to interpolate
+ * 
+ * Perform fluid interpolate 7th order on @buffer and return the result in @output_buffer.
+ * 
+ * Since: 3.8.12
+ */
+void
+ags_fluid_interpolate_7th_order_util_fill_complex(AgsComplex *destination,
+						  AgsComplex *source,
+						  guint buffer_length,
+						  gdouble factor)
+{
+  //TODO:JK: implement me
+}
