62019bd8110f168a7a09cb338ddd194acc2f290d
Joël Krähemann <jkraehemann@gmail.com>
2021-12-12 09:56:04 +0100

    fixed AgsOssDevin

--- ags/audio/oss/ags_oss_devin.c
+++ ags/audio/oss/ags_oss_devin.c
@@ -2221,7 +2221,7 @@ ags_oss_devin_device_record(AgsSoundcard *soundcard,
 
   /* write ring buffer */
   n_write = read(oss_devin->device_fd,
-		 oss_devin->backend_buffer[oss_devin->nth_ring_buffer],
+		 oss_devin->backend_buffer[oss_devin->backend_buffer_mode],
 		 oss_devin->pcm_channels * oss_devin->buffer_size * word_size * sizeof (char));
 
   /* fill buffer */
