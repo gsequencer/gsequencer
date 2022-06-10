2fb7aa9ed1627b21208334d019e45c02f4cdb9b3
Joël Krähemann <jkraehemann@gmail.com>
2022-06-10 13:08:34 +0200

    implemented ::refresh-port() of AgsSF2Synth and AgsSFZSynth

--- ags/app/machine/ags_sfz_synth.c
+++ ags/app/machine/ags_sfz_synth.c
@@ -52,6 +52,8 @@ void ags_sfz_synth_output_map_recall(AgsSFZSynth *sfz_synth,
 				     guint audio_channel_start,
 				     guint output_pad_start);
 
+void ags_sfz_synth_refresh_port(AgsMachine *machine);
+
 /**
  * SECTION:ags_sfz_synth
  * @short_description: sfz_synth
@@ -124,6 +126,8 @@ ags_sfz_synth_class_init(AgsSFZSynthClass *sfz_synth)
   machine = (AgsMachineClass *) sfz_synth;
 
   machine->map_recall = ags_sfz_synth_map_recall;
+
+  machine->refresh_port = ags_sfz_synth_refresh_port;
 }
 
 void
@@ -1190,6 +1194,230 @@ ags_sfz_synth_output_map_recall(AgsSFZSynth *sfz_synth,
   
   sfz_synth->mapped_output_audio_channel = audio_channels;
   sfz_synth->mapped_output_pad = output_pads;
+}
+
+
+void
+ags_sfz_synth_refresh_port(AgsMachine *machine)
+{
+  AgsSFZSynth *sfz_synth;
+  
+  GList *start_play, *start_recall, *recall;
+
+  sfz_synth = (AgsSFZSynth *) machine;
+  
+  start_play = ags_audio_get_play(machine->audio);
+  start_recall = ags_audio_get_recall(machine->audio);
+
+  recall =
+    start_recall = g_list_concat(start_play, start_recall);
+
+  if((recall = ags_recall_find_type(recall, AGS_TYPE_FX_SYNTH_AUDIO)) != NULL){
+    AgsPort *port;
+
+    /* synth octave */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "synth-octave", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->synth_octave,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* synth key */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "synth-key", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->synth_key,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* synth volume */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "synth-volume", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->synth_volume,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }    
+
+    /* chorus enabled */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-enabled", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      if(g_value_get_float(&value) != 0.0){
+	gtk_check_button_set_active(sfz_synth->chorus_enabled,
+				    TRUE);
+      }else{
+	gtk_check_button_set_active(sfz_synth->chorus_enabled,
+				    FALSE);
+      }
+
+      g_object_unref(port);
+    }
+    
+    /* chorus LFO oscillator */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-lfo-oscillator", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      gtk_combo_box_set_active(sfz_synth->chorus_lfo_oscillator,
+			       (gint) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* chorus LFO frequency */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-lfo-frequency", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      gtk_spin_button_set_value(sfz_synth->chorus_lfo_frequency,
+				(gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* chorus depth */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-depth", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->chorus_depth,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* chorus mix */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-mix", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->chorus_mix,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+
+    /* chorus delay */
+    port = NULL;
+
+    g_object_get(recall->data,
+		 "chorus-delay", &port,
+		 NULL);
+
+    if(port != NULL){
+      GValue value = G_VALUE_INIT;
+
+      g_value_init(&value,
+		   G_TYPE_FLOAT);
+
+      ags_port_safe_read(port,
+			 &value);
+
+      ags_dial_set_value(sfz_synth->chorus_delay,
+			 (gdouble) g_value_get_float(&value));
+
+      g_object_unref(port);
+    }
+  }  
 }
 
 /**
