a2cfbf54b97fbb81755606d85f1dee9bed460232
Joël Krähemann <jkraehemann@gmail.com>
2023-03-31 13:11:06 +0200

    fixed type safety with recent clang - modified ags_midi_import_wizard.c and ags_track_collection.c

--- ags/app/import/ags_midi_import_wizard_callbacks.c
+++ ags/app/import/ags_midi_import_wizard_callbacks.c
@@ -1,5 +1,5 @@
 /* GSequencer - Advanced GTK Sequencer
- * Copyright (C) 2005-2022 Joël Krähemann
+ * Copyright (C) 2005-2023 Joël Krähemann
  *
  * This file is part of GSequencer.
  *
@@ -96,7 +96,7 @@ ags_midi_import_wizard_response_callback(GtkWidget *wizard, gint response, gpoin
       ags_ui_provider_set_midi_import_wizard(AGS_UI_PROVIDER(application_context),
 					     NULL);
       
-      gtk_window_destroy(wizard);
+      gtk_window_destroy((GtkWindow *) wizard);
     }
     break;
   default:
