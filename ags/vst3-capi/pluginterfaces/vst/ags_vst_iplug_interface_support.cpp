b81eb801b1d9c786e968111ca0ce7ff63b691595
Joël Krähemann <jkraehemann@gmail.com>
2023-04-19 18:22:26 +0200

    document libags_vst - completed ags_vst_iplug_interface_support.cpp

--- ags/vst3-capi/pluginterfaces/vst/ags_vst_iplug_interface_support.cpp
+++ ags/vst3-capi/pluginterfaces/vst/ags_vst_iplug_interface_support.cpp
@@ -1,5 +1,5 @@
 /* GSequencer - Advanced GTK Sequencer
- * Copyright (C) 2005-2021 Joël Krähemann
+ * Copyright (C) 2005-2023 Joël Krähemann
  *
  * This file is part of GSequencer.
  *
@@ -23,11 +23,27 @@
 
 extern "C" {
 
+  /**
+   * Get IID.
+   *
+   * @return the Steinberg::TUID as AgsVstFUID
+   * 
+   * @since 5.0.0
+   */
   const AgsVstTUID* ags_vst_iplug_interface_support_get_iid()
   {
     return((AgsVstTUID *) &(Steinberg::Vst::IPlugInterfaceSupport::iid.toTUID()));
   }
 
+  /**
+   * Is plug interface supported.
+   * 
+   * @param iplug_interface_support the plug interface support
+   * @param iid the IID
+   * @return the return value
+   * 
+   * @since 5.0.0
+   */
   AgsVstTResult ags_vst_iplug_interface_support_is_plug_interface_supported(AgsVstIPlugInterfaceSupport *iplug_interface_support,
 									    const AgsVstTUID *iid)
   {
