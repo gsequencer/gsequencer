30343aeb36825b57a0d6f8e241275afd9b5c770c
Joël Krähemann <jkraehemann@gmail.com>
2023-04-20 22:00:34 +0200

    document libags_vst - completed ags_vst_bus.cpp

--- ags/vst3-capi/public.sdk/source/vst/ags_vst_bus.cpp
+++ ags/vst3-capi/public.sdk/source/vst/ags_vst_bus.cpp
@@ -25,7 +25,7 @@ extern "C" {
 
 #if 0  
   /**
-   * Wrapper of Bus constructor.
+   * Wrapper of bus constructor.
    * 
    * @param name the name as string
    * @param bus_type the bus type
@@ -40,36 +40,93 @@ extern "C" {
   }
 #endif
   
+  /**
+   * Delete bus.
+   * 
+   * @param bus the bus
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_bus_delete(AgsVstBus *bus)
   {
     delete bus;
   }
 
+  /**
+   * Is bus active.
+   * 
+   * @param bus the bus
+   * @return true if active, otherwise false
+   * 
+   * @since 2.2.0
+   */
   gboolean ags_vst_bus_is_active(AgsVstBus *bus)
   {
     return(static_cast<gboolean>(static_cast<Steinberg::Vst::Bus*>((void *) bus)->isActive()));
   }
 
+  /**
+   * Set bus active according to state.
+   * 
+   * @param bus the bus
+   * @param state true if active else false
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_bus_set_active(AgsVstBus *bus, gboolean state)
   {
     static_cast<Steinberg::Vst::Bus*>((void *) bus)->setActive(static_cast<Steinberg::TBool>(state));
   }
 
+  /**
+   * Set new name of bus.
+   * 
+   * @param bus the bus
+   * @param new_name the new name as string
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_bus_set_name(AgsVstBus *bus, AgsVstString new_name)
   {
     static_cast<Steinberg::Vst::Bus*>((void *) bus)->setName(static_cast<Steinberg::String>((gchar *) new_name));
   }
 
+  /**
+   * Set new bus type of bus.
+   * 
+   * @param bus the bus
+   * @param new_bus_type the bus type
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_bus_set_bus_type(AgsVstBus *bus, AgsVstBusType new_bus_type)
   {
     static_cast<Steinberg::Vst::Bus*>((void *) bus)->setBusType(static_cast<Steinberg::Vst::BusType>(new_bus_type));
   }
 
+  /**
+   * Set new flags of bus.
+   * 
+   * @param bus the bus
+   * @param new_flags the new flags
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_bus_set_flags(AgsVstBus *bus, guint32 new_flags)
   {
     static_cast<Steinberg::Vst::Bus*>((void *) bus)->setFlags(new_flags);
   }
 
+  /**
+   * Get info of bus.
+   * 
+   * @param bus the bus
+   * @param info the bus info
+   * @return true on success, otherwise false
+   * 
+   * 
+   * Since: 2.2.0
+   */
   gboolean ags_vst_bus_get_info(AgsVstBus *bus, AgsVstBusInfo *info)
   {
     Steinberg::Vst::BusInfo *tmp_info_0 = static_cast<Steinberg::Vst::BusInfo*>((void *) info);
@@ -78,16 +135,44 @@ extern "C" {
     return(static_cast<gboolean>(static_cast<Steinberg::Vst::Bus*>((void *) bus)->getInfo(const_cast<Steinberg::Vst::BusInfo&>(tmp_info_1))));
   }
 
+  /**
+   * Instantiate new Steinberg::Vst::EventBus the constructor as a C99
+   * compatible handle a void pointer.
+   * 
+   * @param name the name as string
+   * @param bus_type the bus type
+   * @param flags the flags
+   * @param channel_count the channel count
+   * @return the new instance of event bus
+   * 
+   * @since 2.2.0
+   */
   AgsVstEventBus* ags_vst_event_bus_new(AgsVstTChar *name, AgsVstBusType bus_type, gint32 flags, gint32 channel_count)
   {
     return((AgsVstEventBus *) new Steinberg::Vst::EventBus((char16_t *) name, static_cast<Steinberg::Vst::BusType>(bus_type), flags, channel_count));
   }
 
+  /**
+   * Delete.
+   *
+   * @param event_bus the event bus
+   *
+   * @since 2.2.0
+   */
   void ags_vst_event_bus_delete(AgsVstEventBus *event_bus)
   {
     delete event_bus;
   }
 
+  /**
+   * Get info of bus.
+   * 
+   * @param bus the event bus
+   * @param info the bus info
+   * @return true on success, otherwise false
+   * 
+   * Since: 2.2.0
+   */
   gboolean ags_vst_event_bus_get_info(AgsVstEventBus *event_bus, AgsVstBusInfo *info)
   {
     Steinberg::Vst::BusInfo *tmp_info_0 = static_cast<Steinberg::Vst::BusInfo*>((void *) info);
@@ -96,26 +181,70 @@ extern "C" {
     return(static_cast<gboolean>(static_cast<Steinberg::Vst::EventBus*>((void *) event_bus)->getInfo(const_cast<Steinberg::Vst::BusInfo&>(tmp_info_1))));
   }
 
+  /**
+   * Instantiate new Steinberg::Vst::AudioBus the constructor as a C99
+   * compatible handle a void pointer.
+   * 
+   * @param name the name as string
+   * @param bus_type the bus type
+   * @param flags the flags
+   * @param arr the speaker arrangement
+   * @return the new instance of audio bus
+   * 
+   * @since 2.2.0
+   */
   AgsVstAudioBus* ags_vst_audio_bus_new(AgsVstTChar *name, AgsVstBusType bus_type, gint32 flags, AgsVstSpeakerArrangement arr)
   {
     return((AgsVstAudioBus *) new Steinberg::Vst::AudioBus((char16_t *) name, static_cast<Steinberg::Vst::BusType>(bus_type), flags, arr));
   }
 
+  /**
+   * Delete.
+   *
+   * @param audio_bus the audio bus
+   *
+   * @since 2.2.0
+   */
   void ags_vst_audio_bus_delete(AgsVstAudioBus *audio_bus)
   {
     delete audio_bus;
   }
 
+  /**
+   * Get speaker arrangement of audio bus.
+   * 
+   * @param audio_bus the audio bus
+   * @return the speaker arrangement 
+   * 
+   * @since 2.2.0
+   */
   AgsVstSpeakerArrangement ags_vst_audio_bus_get_arrangement(AgsVstAudioBus *audio_bus)
   {
     return((AgsVstSpeakerArrangement) static_cast<Steinberg::Vst::AudioBus*>((void *) audio_bus)->getArrangement());
   }
 
+  /**
+   * Set speaker arrangement of audio bus.
+   * 
+   * @param audio_bus the audio bus
+   * @param arr the speaker arrangement 
+   * 
+   * @since 2.2.0
+   */
   void ags_vst_audio_bus_set_arrangement(AgsVstAudioBus *audio_bus, AgsVstSpeakerArrangement arr)
   {
     static_cast<Steinberg::Vst::AudioBus*>((void *) audio_bus)->setArrangement(static_cast<Steinberg::Vst::SpeakerArrangement>(arr));
   }
 
+  /**
+   * Get @info of @bus.
+   * 
+   * @param bus the audio bus
+   * @param info the bus info
+   * #return true on success, otherwise false
+   * 
+   * @since 2.2.0
+   */
   gboolean ags_vst_audio_bus_get_info(AgsVstAudioBus *audio_bus, AgsVstBusInfo *info)
   {
     Steinberg::Vst::BusInfo *tmp_info_0 = static_cast<Steinberg::Vst::BusInfo*>((void *) info);
@@ -124,21 +253,54 @@ extern "C" {
     return(static_cast<gboolean>(static_cast<Steinberg::Vst::AudioBus*>((void *) audio_bus)->getInfo(const_cast<Steinberg::Vst::BusInfo&>(tmp_info_1))));
   }
 
+  /**
+   * Instantiate new Steinberg::Vst::BusList the constructor as a C99
+   * compatible handle a void pointer.
+   * 
+   * @param type the media type
+   * @param dir the bus direction
+   * @return the new instance of bus list
+   * 
+   * @since 2.2.0
+   */
   AgsVstBusList* ags_vst_bus_list_new(AgsVstMediaType type, AgsVstBusDirection dir)
   {
     return((AgsVstBusList *) new Steinberg::Vst::BusList(type, dir));
   }
   
+  /**
+   * Delete.
+   *
+   * @param bus_list the bus list
+   *
+   * @since 2.2.0
+   */
   void ags_vst_bus_list_delete(AgsVstBusList *bus_list)
   {
     delete bus_list;
   }
 
+  /**
+   * Get media type of @bus_list.
+   * 
+   * @param bus_list the bus list
+   * @return the media type 
+   * 
+   * @since 2.2.0
+   */
   AgsVstMediaType ags_vst_bus_list_get_type(AgsVstBusList *bus_list)
   {
     return(static_cast<Steinberg::Vst::BusList*>((void *) bus_list)->getType());
   }
 
+  /**
+   * Get bus direction of bus list.
+   * 
+   * @param bus_list the bus list
+   * @return the bus direction
+   * 
+   * @ince 2.2.0
+   */
   AgsVstBusDirection ags_vst_bus_list_get_direction(AgsVstBusList *bus_list)
   {
     return(static_cast<Steinberg::Vst::BusList*>((void *) bus_list)->getDirection());
--- ags/vst3-capi/public.sdk/source/vst/ags_vst_bus.h
+++ ags/vst3-capi/public.sdk/source/vst/ags_vst_bus.h
@@ -37,233 +37,46 @@ extern "C" {
 
   AgsVstBus* ags_vst_bus_new(AgsVstTChar *name, AgsVstBusType bus_type, gint32 flags);
 
-  /**
-   * ags_vst_bus_delete:
-   * @bus: the #AgsVstBus-struct pointer
-   * 
-   * Delete @bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_delete(AgsVstBus *bus);
 
-  /**
-   * ags_vst_bus_is_active:
-   * @bus: the #AgsVstBus-struct pointer
-   * 
-   * Is @bus active.
-   * 
-   * Returns: %TRUE if active, otherwise %FALSE
-   * 
-   * Since: 2.2.0
-   */
   gboolean ags_vst_bus_is_active(AgsVstBus *bus);
-
-  /**
-   * ags_vst_bus_set_active:
-   * @bus: the #AgsVstBus-struct pointer
-   * @state: %TRUE if active else %FALSE
-   * 
-   * Set @bus active according to @state.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_set_active(AgsVstBus *bus, gboolean state);
 
-  /**
-   * ags_vst_bus_set_name:
-   * @bus: the #AgsVstBus-struct pointer
-   * @new_name: the new name as string
-   * 
-   * Set @new_name of @bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_set_name(AgsVstBus *bus, AgsVstString new_name);
 
-  /**
-   * ags_vst_bus_set_bus_type:
-   * @bus: the #AgsVstBus-struct pointer
-   * @new_bus_type: the #AgsVstBusType-enum
-   * 
-   * Set @new_bus_type of @bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_set_bus_type(AgsVstBus *bus, AgsVstBusType new_bus_type);
 
-  /**
-   * ags_vst_bus_set_flags:
-   * @bus: the #AgsVstBus-struct pointer
-   * @new_flags: the new flags
-   * 
-   * Set @new_flags of @bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_set_flags(AgsVstBus *bus, guint32 new_flags);
 
-  /**
-   * ags_vst_bus_get_info:
-   * @bus: the #AgsVstBus-struct pointer
-   * @info: the #AgsVstBusInfo-struct pointer to a pointer
-   * 
-   * Get @info of @bus.
-   * 
-   * Returns: %TRUE on success, otherwise %FALSE
-   * 
-   * Since: 2.2.0
-   */
   gboolean ags_vst_bus_get_info(AgsVstBus *bus, AgsVstBusInfo *info);
 
   typedef struct AgsVstEventBus EventBus;
 
-  /**
-   * ags_vst_event_bus_new:
-   * @name: the name as string
-   * @bus_type: the #AgsVstBusType-enum
-   * @flags: the flags
-   * @channel_count: the channel count
-   * 
-   * Wrapper of EventBus constructor.
-   * 
-   * Returns: the #AgsVstEventBus-struct pointer
-   * 
-   * Since: 2.2.0
-   */
   AgsVstEventBus* ags_vst_event_bus_new(AgsVstTChar *name, AgsVstBusType bus_type, gint32 flags, gint32 channel_count);
 
-  /**
-   * ags_vst_event_bus_delete:
-   * @event_bus: the #AgsVstEventBus-struct pointer
-   * 
-   * Delete @event_bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_event_bus_delete(AgsVstEventBus *event_bus);
 
-  /**
-   * ags_vst_event_bus_get_info:
-   * @bus: the #AgsVstEventBus-struct pointer
-   * @info: the #AgsVstBusInfo-struct pointer to a pointer
-   * 
-   * Get @info of @bus.
-   * 
-   * Returns: %TRUE on success, otherwise %FALSE
-   * 
-   * Since: 2.2.0
-   */
   gboolean ags_vst_event_bus_get_info(AgsVstEventBus *event_bus, AgsVstBusInfo *info);
 
   typedef struct AgsVstAudioBus AudioBus;
 
-  /**
-   * ags_vst_audio_bus_new:
-   * @name: the name as string
-   * @bus_type: the #AgsVstBusType-enum
-   * @flags: the flags
-   * @arr: the speaker arrangement
-   * 
-   * Wrapper of AudioBus constructor.
-   * 
-   * Returns: the #AgsVstAudioBus-struct pointer
-   * 
-   * Since: 2.2.0
-   */
   AgsVstAudioBus* ags_vst_audio_bus_new(AgsVstTChar *name, AgsVstBusType bus_type, gint32 flags, AgsVstSpeakerArrangement arr);
 
-  /**
-   * ags_vst_audio_bus_delete:
-   * @audio_bus: the #AgsVstAudioBus-struct pointer
-   * 
-   * Delete @audio_bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_audio_bus_delete(AgsVstAudioBus *audio_bus);
 
-  /**
-   * ags_vst_audio_bus_get_arrangement:
-   * @audio_bus: the #AgsVstAudioBus-struct pointer
-   * 
-   * Get speaker arrangement of @audio_bus.
-   * 
-   * Returns: the speaker arrangement 
-   * 
-   * Since: 2.2.0
-   */
   AgsVstSpeakerArrangement ags_vst_audio_bus_get_arrangement(AgsVstAudioBus *audio_bus);
-
-  /**
-   * ags_vst_audio_bus_set_arrangement:
-   * @audio_bus: the #AgsVstAudioBus-struct pointer
-   * @arr: the speaker arrangement location
-   * 
-   * Set @arr of @audio_bus.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_audio_bus_set_arrangement(AgsVstAudioBus *audio_bus, AgsVstSpeakerArrangement arr);
 
-  /**
-   * ags_vst_audio_bus_get_info:
-   * @bus: the #AgsVstAudioBus-struct pointer
-   * @info: the #AgsVstBusInfo-struct pointer to a pointer
-   * 
-   * Get @info of @bus.
-   * 
-   * Returns: %TRUE on success, otherwise %FALSE
-   * 
-   * Since: 2.2.0
-   */
   gboolean ags_vst_audio_bus_get_info(AgsVstAudioBus *audio_bus, AgsVstBusInfo *info);
   
   typedef struct AgsVstBusList BusList;
 
-  /**
-   * ags_vst_bus_list_new:
-   * 
-   * Wrapper of BusList constructor.
-   * 
-   * Returns: the #AgsVstBusList-struct pointer
-   * 
-   * Since: 2.2.0
-   */
   AgsVstBusList* ags_vst_bus_list_new(AgsVstMediaType type, AgsVstBusDirection dir);
 
-  /**
-   * ags_vst_bus_list_delete:
-   * @bus_list: the #AgsVstBusList-struct pointer
-   * 
-   * Delete @bus_list.
-   * 
-   * Since: 2.2.0
-   */
   void ags_vst_bus_list_delete(AgsVstBusList *bus_list);
 
-  /**
-   * ags_vst_bus_list_get_type:
-   * @bus_list: the #AgsVstBusList-struct pointer
-   * 
-   * Get media type of @bus_list.
-   * 
-   * Returns: the AgsVstMediaType
-   * 
-   * Since: 2.2.0
-   */
   AgsVstMediaType ags_vst_bus_list_get_type(AgsVstBusList *bus_list);
 
-  /**
-   * ags_vst_bus_list_get_direction:
-   * @bus_list: the #AgsVstBusList-struct pointer
-   * 
-   * Get bus direction of @bus_list.
-   * 
-   * Returns: the AgsVstBusDirection
-   * 
-   * Since: 2.2.0
-   */
   AgsVstBusDirection ags_vst_bus_list_get_direction(AgsVstBusList *bus_list);
   
 #ifdef __cplusplus
