bce6b17bee722559272ff5da0d0ee03cc259f2cc
Joël Krähemann <jkraehemann@gmail.com>
2023-07-30 13:19:03 +0200

    add tests to ags_midi_ci_util_test.c

--- ags/audio/midi/ags_midi_ci_util.h
+++ ags/audio/midi/ags_midi_ci_util.h
@@ -293,18 +293,18 @@ void ags_midi_ci_util_put_profile_inquiry_reply(AgsMidiCIUtil *midi_ci_util,
 						AgsMUID source,
 						AgsMUID destination,
 						gint16 enabled_profile_count,
-						guchar **enabled_profile,
+						guchar* enabled_profile[5],
 						gint16 disabled_profile_count,
-						guchar **disabled_profile);
+						guchar* disabled_profile[5]);
 guint ags_midi_ci_util_get_profile_inquiry_reply(AgsMidiCIUtil *midi_ci_util,
 						 guchar *buffer,
 						 guchar *version,
 						 AgsMUID *source,
 						 AgsMUID *destination,
 						 gint16 *enabled_profile_count,
-						 guchar **enabled_profile,
+						 guchar ***enabled_profile,
 						 gint16 *disabled_profile_count,
-						 guchar **disabled_profile);
+						 guchar ***disabled_profile);
 
 void ags_midi_ci_util_put_profile_enabled_report(AgsMidiCIUtil *midi_ci_util,
 						 guchar *buffer,
