6b7dce27efd71d799f7da4dd8eab26593a1d73dc
Joël Krähemann <jkraehemann@gmail.com>
2023-09-11 03:52:29 +0100

    fixed double free

--- ags/audio/fx/ags_fx_vst3_audio.c
+++ ags/audio/fx/ags_fx_vst3_audio.c
@@ -657,6 +657,9 @@ ags_fx_vst3_audio_notify_samplerate_callback(GObject *gobject,
   if(recall_container != NULL){
     g_object_unref(recall_container);
   }
+
+  g_list_free_full(start_recall_channel,
+		   (GDestroyNotify) g_object_unref);
 }
 
 void
@@ -870,6 +873,9 @@ ags_fx_vst3_audio_set_audio_channels_callback(AgsAudio *audio,
   if(recall_container != NULL){
     g_object_unref(recall_container);
   }
+
+  g_list_free_full(start_recall_channel,
+		   (GDestroyNotify) g_object_unref);
 }
 
 void
@@ -956,6 +962,9 @@ ags_fx_vst3_audio_set_pads_callback(AgsAudio *audio,
   if(recall_container != NULL){
     g_object_unref(recall_container);
   }
+
+  g_list_free_full(start_recall_channel,
+		   (GDestroyNotify) g_object_unref);
 }
 
 /**
@@ -2170,7 +2179,7 @@ ags_fx_vst3_audio_load_port(AgsFxVst3Audio *fx_vst3_audio)
     g_object_unref(recall_container);
   }
 
-  g_list_free_full(start_plugin_port,
+  g_list_free_full(start_recall_channel,
 		   (GDestroyNotify) g_object_unref);
 
   g_list_free_full(start_plugin_port,
