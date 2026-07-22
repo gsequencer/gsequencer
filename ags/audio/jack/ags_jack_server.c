b8fd3b21d21fe1135eb62edca168452d7226ae33
Joël Krähemann <jkraehemann@gmail.com>
2026-07-22 14:17:45 +0200

    fixed input soundcard preferences

--- ags/audio/jack/ags_jack_server.c
+++ ags/audio/jack/ags_jack_server.c
@@ -1257,6 +1257,8 @@ ags_jack_server_register_soundcard_with_params(AgsSoundServer *sound_server,
     gchar *backend;
     gchar *device;
 
+    gboolean is_output;
+
     if(!g_key_file_has_group(config->key_file,
 			     soundcard_group)){
       if(i == 0){
